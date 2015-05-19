1c299f21b429bbdb545065c3ca3fb44d7c05f8f7
John Benson <john.benson@bluespurs.com>
2015-05-19 16:18:19 -0300

    TITAN-1043 Migrating the navbar to a directive.

---
+++ src/app/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('liveopsConfigPanel')
+    .directive('navbar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/components/navbar/navbar.html',
+            controller: 'NavbarCtrl'
+        }
+    });
