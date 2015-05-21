7b99e5a19530e08cc6f8d11012156bd8614c3179
Phil Hachey <phil.hachey@bluespurs.com>
2015-05-21 10:34:41 -0300

    TITAN-1040 added some styling and ngClick events for selected user

--- src/app/components/users/userList/userListController.js
+++ src/app/components/users/userList/userListController.js
@@ -4,6 +4,19 @@ angular.module('liveopsConfigPanel')
   .controller('UserListController', ['$scope', 'UserService', function ($scope, UserService) {
     UserService.query(function(data){
       $scope.users = data.result;
+      
+      if($scope.users) {
+        $scope.selectedUser = $scope.users[0];
+        $scope.selectedUser.display = {
+          firstName : $scope.selectedUser.firstName,
+          lastName : $scope.selectedUser.lastName,
+          displayName : $scope.selectedUser.displayName
+        }
+      }
+      
+      $scope.selectUser = function(user) {
+        $scope.selectedUser = user;
+      }
     });
 
   }]);
