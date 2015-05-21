3b66e667b2682728f19df643577cb01174fb1405
Phil Hachey <phil.hachey@bluespurs.com>
2015-05-21 10:47:13 -0300

    Added config default for local and fixed build warnings

--- gulp/constant.js
+++ gulp/constant.js
@@ -6,7 +6,7 @@ var ngConstant = require('gulp-ng-constant');
 module.exports = function(options) {
   gulp.task('config', function () {
     var myConfig = require('../config.json');
-    var envConfig = myConfig[process.env.ENV];
+    var envConfig = myConfig[process.env.ENV || 'local'];
     return ngConstant({
         name: 'liveopsConfigPanel.config',
         constants: envConfig,
