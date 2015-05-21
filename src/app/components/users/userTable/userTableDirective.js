386ec98b111136444bacd42f27787116fdf76d00
Sasha Wilcox <sasha.wilcox@bluespurs.com>
2015-05-21 12:03:04 -0300

    TITAN-1042 Initial user search by displayName and location with * as wildcard

--- src/app/components/users/userTable/userTableDirective.js
+++ src/app/components/users/userTable/userTableDirective.js
@@ -6,6 +6,24 @@ angular.module('liveopsConfigPanel')
       scope: {
         users: '=users'
       },
+      restrict:'E',
+
+      link : function (scope) {
+        scope.searchUser = function (user) {
+          if (!scope.queryUser) return true;
+          var wildCardQuery = new RegExp(scope.regExpReplace(scope.queryUser), 'i');
+
+          // Search by displayName and location
+          return (wildCardQuery.test(user.displayName) || wildCardQuery.test(user.location));
+        };
+
+        scope.regExpReplace = function (string){
+          // Allow all characters in user search, use * as wildcard
+          string.replace(/([.+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+          return string.replace(/([*])/g, '.*');
+        };
+
+      },
       templateUrl: 'app/components/users/userTable/userTable.html'
     };
  });
