a50658a9bf493cd2fcb7deb228605f3374c6fa9e
samantha.routledge <samantha.routledge@bluespurs.com>
2015-05-22 14:48:17 -0300

    TITAN-1039 Optimized event binding for the drop down a bit.

--- src/app/shared/directives/dropdown/dropdownDirective.js
+++ src/app/shared/directives/dropdown/dropdownDirective.js
@@ -3,46 +3,58 @@
 angular.module('liveopsConfigPanel')
   .directive('dropdown', ['$document', function($document) {
     return {
-    scope : {
-      items : '=',
-      label : '@',
-      changeFunction : '=changefunction'
-    },
-    templateUrl : 'app/shared/directives/dropdown/dropdown.html',
-    link : function(scope, element) {
-      scope.showDrop = false;
-      
-      $document.bind('click', function(event) {
-        var clickedInDropdown = element.find(event.target).length > 0;
-        if (clickedInDropdown) {
-          return;
-        }
-        
-        scope.$apply(function() {
-          scope.showDrop = false;
-        });
-      });
-      
-      scope.$watch(function(scope) {return scope.items.all.checked;}, 
-        function(newValue, oldValue) {
+      scope : {
+        items : '=',
+        label : '@',
+        changeFunction : '=changefunction'
+      },
+      templateUrl : 'app/shared/directives/dropdown/dropdown.html',
+      link : function(scope, element) {
+        scope.showDrop = false;
+
+        //Only bother listening for the click event when a dropdown is open
+        scope.$watch(function() {return scope.showDrop;}, function(newValue, oldValue) {
           if (newValue && !oldValue) {
-            angular.forEach(scope.items.filters, function(state) {
-              state.checked = false;
-            });
-          }
-        }
-      );
-      
-      angular.forEach(scope.items.filters, function(state) {
-        scope.$watch(function() {
-          return state.checked;
-        }, function(newValue, oldValue) {
-          if (newValue && !oldValue) {
-            scope.items.all.checked = false;
+            $document.on('click', onClick);
           }
         });
-      });
-    }
+
+        function onClick(event){
+          //Hide the dropdown when user clicks outside of it
+          var clickedInDropdown = element.find(event.target).length > 0;
+          if (clickedInDropdown) {
+            return;
+          }
+
+          scope.$apply(function() {
+            scope.showDrop = false;
+          });
+
+          $document.off('click', onClick);
+        }
+
+        //Automatically uncheck other filters when "All" is selected
+        scope.$watch(function(scope) {return scope.items.all.checked;},
+          function(newValue, oldValue) {
+            if (newValue && !oldValue) {
+              angular.forEach(scope.items.filters, function(state) {
+                state.checked = false;
+              });
+            }
+          }
+        );
+
+        //Automatically uncheck "All" when another filter is selected
+        angular.forEach(scope.items.filters, function(state) {
+          scope.$watch(function() {
+            return state.checked;
+          }, function(newValue, oldValue) {
+            if (newValue && !oldValue) {
+              scope.items.all.checked = false;
+            }
+          });
+        });
+      }
     };
    }])
 ;
--- src/app/shared/directives/resizeHandle/resizeHandleDirective.js
+++ src/app/shared/directives/resizeHandle/resizeHandleDirective.js
@@ -1,4 +1,5 @@
 'use strict';
+/*jslint browser:true */
 
 angular.module('liveopsConfigPanel')
   .directive('resizeHandle', ['$window', '$document', function($window, $document) {
@@ -9,22 +10,22 @@ angular.module('liveopsConfigPanel')
         maxWidth : '@',
         minWidth : '@'
       },
-      
+
       templateUrl : 'app/shared/directives/resizeHandle/resizeHandle.html',
       link : function(scope, element) {
         //Need non-jqlite object here because jqlite doesn't support offsetLeft, used in mousemove()
         scope.targetElement = document.getElementById(scope.elementId);
-        
+
         element.on('mousedown', function(event) {
           //Don't initiate resize on right click, because it's annoying
           if (event.button !== 2) {
             event.preventDefault();
-            
+
             $document.on('mousemove', mousemove);
             $document.on('mouseup', mouseup);
           }
         });
-        
+
         function mousemove(event) {
           var left = scope.targetElement.offsetLeft;
           var x = event.pageX;
@@ -33,17 +34,17 @@ angular.module('liveopsConfigPanel')
           if (scope.minWidth && newWidth < scope.minWidth) {
             return;
           }
-          
+
           // Restrict by maxWidth if it was given,
           // otherwise make sure it doesn't get dragged larger than
           // the page, which would make the handle unreachable.
           if ((scope.maxWidth && newWidth > scope.maxWidth) || newWidth > ($window.innerWidth * 0.95)) {
             return;
           }
-          
+
           angular.element(scope.targetElement).css('width', newWidth + 'px');
         }
-        
+
         function mouseup() {
           $document.unbind('mousemove', mousemove);
           $document.unbind('mouseup', mouseup);
