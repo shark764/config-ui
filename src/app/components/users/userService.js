08b61a903ab462655e1bfca740563fa2580ed8fe
Greg Royan <greg.royan@bluespurs.com>
2015-05-26 09:45:19 -0300

    Titan-1041 - Update to match imported values to variable names

--- src/app/components/users/userService.js
+++ src/app/components/users/userService.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-.factory('UserService', ['$resource', 'apiHostname', 'Session', function ($resource, hostname, session) {
-  return $resource('http://' + hostname + '/v1/users/:id', {}, {
+.factory('UserService', ['$resource', 'apiHostname', 'Session', function ($resource, apiHostname, Session) {
+  return $resource('http://' + apiHostname + '/v1/users/:id', {}, {
     //API response is always an object, but Resource expects an array in response to query(). This config overrides.
     query: { method: 'GET', isArray: false },
     update: { method: 'PUT',
       transformRequest: function (data, headers){
-        data.updatedBy = session.id;
+        data.updatedBy = Session.id;
         return JSON.stringify(data);
       }
     },
     save: { method: "POST",
       transformRequest: function (data, headers) {
-        data.createdBy = session.id;
+        data.createdBy = Session.id;
         return JSON.stringify(data);
       }
     }
