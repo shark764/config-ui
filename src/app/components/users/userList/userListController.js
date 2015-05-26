a298586c7b3f6af12fb9f69f5e20e756b35334bf
samantha.routledge <samantha.routledge@bluespurs.com>
2015-05-26 13:46:31 -0300

    TITAN-1039 fixed style conflict with status/state filters and select drop down. Fixed dumb rebase.

--- src/app/components/users/userList/userListController.js
+++ src/app/components/users/userList/userListController.js
@@ -5,7 +5,6 @@ angular.module('liveopsConfigPanel')
     $scope.showModal = false;
     $scope.showError = false;
     $scope.errorMsg = 'Input required data';
-    $scope.checkedUsers = [];
     
     $scope.selectUser = function (user) {
       $scope.selectedUserContext = {
@@ -25,42 +24,10 @@ angular.module('liveopsConfigPanel')
       $scope.selectedUserContext = {};
 
       if (data.result) {
-        // Binding form to first result. Putting this in for now.
-        $scope.selectUser(data.result[0]);
-          
-        // Watch the checked value for items in the users list, so we can
-        // autoupdate the checkedUsers list when needed
-        angular.forEach($scope.users, function(user) {
-          $scope.$watch(function() {return user.checked;}, function() {
-            $scope.checkedUsers = filterFilter($scope.filteredUsers, {'checked' : true});
-          });
-        });
-        
         // Watch the search value so we can update the filteredUsers list which
         // gets passed to the table directive
         $scope.$watch(function() {return $scope.queryUser;}, function() {
           $scope.filteredUsers = $filter('UserSearchFilter')($scope.users, $scope.queryUser);
-        });
-        
-        // Watch for additions to the users list
-        $scope.$watchCollection('users', function(newList, oldList) {
-            if (newList < oldList){
-              return;
-            }
-            
-            var newItems = [];
-            angular.forEach(newList, function(user) {
-              if (oldList.indexOf(user) === -1) {
-                newItems.push(user);
-              }
-            });
-            
-            // Add a watch to the new user(s) checked attribute
-            angular.forEach(newItems, function(user) {
-              $scope.$watch(function() {return user.checked;}, function() {
-                $scope.checkedUsers = filterFilter($scope.filteredUsers, {'checked' : true});
-              });
-            });
         });
       }
   	});
@@ -68,6 +35,15 @@ angular.module('liveopsConfigPanel')
     $scope.$on('userTable:user:selected', function (event, selectedUser) {
       $scope.selectedUser = selectedUser;
       $scope.$broadcast('userList:user:selected', selectedUser);
+    });
+    
+    $scope.hasChecked = 0;
+    $scope.$on('userList:user:checked', function () {
+      $scope.hasChecked++;
+    });
+    
+    $scope.$on('userList:user:unchecked', function () {
+      $scope.hasChecked = $scope.hasChecked > 0 ? $scope.hasChecked - 1 : 0;
     });
     
   	$scope.selectOptions = [{
@@ -83,19 +59,40 @@ angular.module('liveopsConfigPanel')
         }
     }];
   	
-  	
   	$scope.selectAll = function(){
+  	  $scope.hasChecked = $scope.users.length;
   	  angular.forEach($scope.users, function(user) {
-        user.checked = true;
+  	    if (!user.filtered){
+  	      user.checked = true;
+  	    }
       });
   	};
   	
   	$scope.selectNone = function(){
+  	  $scope.hasChecked = 0;
   	  angular.forEach($scope.users, function(user) {
         user.checked = false;
       });
   	};
   	
+  	$scope.enableChecked = function(){
+      angular.forEach($scope.filteredUsers, function(user) {
+        if (user.checked){
+          $scope.saveUser({'status' : true}, user.id);
+          user.status = true;
+        }
+      });
+    };
+    
+    $scope.disableChecked = function(){
+      angular.forEach($scope.filteredUsers, function(user) {
+        if (user.checked){
+          $scope.saveUser({'status' : false}, user.id);
+          user.status = false;
+        }
+      });
+    };
+    
     $scope.showModalSection = function(){
     	$scope.showModal = true;
     };
@@ -131,38 +128,6 @@ angular.module('liveopsConfigPanel')
         id: userId
       }, data).$promise;
     };
-
-    $scope.updateUser = function(userId, data){
-      UserService.update( { id:userId }, data)
-        .$promise.then(
-          $scope.successResponse,
-          $scope.errorResponse
-        );
-    };
-
-    $scope.enableChecked = function(){
-      angular.forEach($scope.checkedUsers, function(user) {
-        $scope.saveUser({'status' : true}, user.id);
-        user.status = true;
-      });
-    };
-    
-    $scope.disableChecked = function(){
-      angular.forEach($scope.checkedUsers, function(user) {
-        $scope.saveUser({'status' : false}, user.id);
-        user.status = false;
-      });
-    };
-
-    $scope.successResponse = function () {
-      $scope.showError = false;
-      $scope.showModal = false;
-    };
-
-    $scope.errorResponse = function (data) {
-      $scope.showError = true;
-      $scope.errorMsg = data.data.message;
-    };
     
     $scope.$on('editField:save', function (event, args) {
       var saveObject = {};
@@ -194,11 +159,14 @@ angular.module('liveopsConfigPanel')
       var filtered = [];
       angular.forEach(users, function(user) {
         if ((wildCardQuery.test(user.firstName + ' ' + user.lastName))){
+          user.filtered = false;
           filtered.push(user);
         } else {
-          // Uncheck users that have been excluded by the search, so they are
-          // not included in batch operations:
-          user.checked = false;
+          user.filtered = true;
+          if (user.checked){
+            scope.$emit('userList:user:unchecked');
+            user.checked = false;
+          }
         }
       });
 
