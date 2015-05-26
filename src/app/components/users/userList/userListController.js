7479d099cb38537663bbad5869493f083edbfb6c
John Benson <john.benson@bluespurs.com>
2015-05-26 11:36:10 -0300

    TITAN-1039b Added wrapper around dropdown.

--- src/app/components/users/userList/userListController.js
+++ src/app/components/users/userList/userListController.js
@@ -6,7 +6,7 @@ angular.module('liveopsConfigPanel')
     $scope.showError = false;
     $scope.errorMsg = 'Input required data';
     $scope.checkedUsers = [];
-    
+
     $scope.selectUser = function (user) {
       $scope.selectedUserContext = {
         user: user
@@ -27,7 +27,7 @@ angular.module('liveopsConfigPanel')
       if (data.result) {
         // Binding form to first result. Putting this in for now.
         $scope.selectUser(data.result[0]);
-          
+
         // Watch the checked value for items in the users list, so we can
         // autoupdate the checkedUsers list when needed
         angular.forEach($scope.users, function(user) {
@@ -35,26 +35,26 @@ angular.module('liveopsConfigPanel')
             $scope.checkedUsers = filterFilter($scope.filteredUsers, {'checked' : true});
           });
         });
-        
+
         // Watch the search value so we can update the filteredUsers list which
         // gets passed to the table directive
         $scope.$watch(function() {return $scope.queryUser;}, function() {
           $scope.filteredUsers = $filter('UserSearchFilter')($scope.users, $scope.queryUser);
         });
-        
+
         // Watch for additions to the users list
         $scope.$watchCollection('users', function(newList, oldList) {
             if (newList < oldList){
               return;
             }
-            
+
             var newItems = [];
             angular.forEach(newList, function(user) {
               if (oldList.indexOf(user) === -1) {
                 newItems.push(user);
               }
             });
-            
+
             // Add a watch to the new user(s) checked attribute
             angular.forEach(newItems, function(user) {
               $scope.$watch(function() {return user.checked;}, function() {
@@ -69,33 +69,33 @@ angular.module('liveopsConfigPanel')
       $scope.selectedUser = selectedUser;
       $scope.$broadcast('userList:user:selected', selectedUser);
     });
-    
+
   	$scope.selectOptions = [{
     	  label : 'All',
     	  onClick : function(){
     	      $scope.selectAll();
     	  }
-    	}, 
+    	},
     	{
     	 label : 'None',
        onClick : function(){
          $scope.selectNone();
         }
     }];
-  	
-  	
+
+
   	$scope.selectAll = function(){
   	  angular.forEach($scope.users, function(user) {
         user.checked = true;
       });
   	};
-  	
+
   	$scope.selectNone = function(){
   	  angular.forEach($scope.users, function(user) {
         user.checked = false;
       });
   	};
-  	
+
     $scope.showModalSection = function(){
     	$scope.showModal = true;
     };
@@ -132,21 +132,13 @@ angular.module('liveopsConfigPanel')
       }, data).$promise;
     };
 
-    $scope.updateUser = function(userId, data){
-      UserService.update( { id:userId }, data)
-        .$promise.then(
-          $scope.successResponse,
-          $scope.errorResponse
-        );
-    };
-
     $scope.enableChecked = function(){
       angular.forEach($scope.checkedUsers, function(user) {
         $scope.saveUser({'status' : true}, user.id);
         user.status = true;
       });
     };
-    
+
     $scope.disableChecked = function(){
       angular.forEach($scope.checkedUsers, function(user) {
         $scope.saveUser({'status' : false}, user.id);
@@ -163,7 +155,7 @@ angular.module('liveopsConfigPanel')
       $scope.showError = true;
       $scope.errorMsg = data.data.message;
     };
-    
+
     $scope.$on('editField:save', function (event, args) {
       var saveObject = {};
       saveObject.updatedBy = '1c838030-f772-11e4-ac37-45b2e1245d4b';
@@ -183,12 +175,12 @@ angular.module('liveopsConfigPanel')
       string.replace(/([.+?^=!:${}()|\[\]\/\\])/g, '\\$1');
       return string.replace(/([*])/g, '.*');
     }
-    
+
     return function(users, query) {
       if (! query){
         return users;
       }
-      
+
       var wildCardQuery = new RegExp(regExpReplace(query), 'i');
 
       var filtered = [];
