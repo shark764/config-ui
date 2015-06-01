681b977d0bc223c3acba037f26e38b53b8e53766
samantha.routledge <samantha.routledge@bluespurs.com>
2015-06-01 10:04:30 -0300

    filter

--- src/app/components/users/userTable/userTableDirective.js
+++ src/app/components/users/userTable/userTableDirective.js
@@ -16,7 +16,7 @@ angular.module('liveopsConfigPanel')
           if($routeParams.id) {
             var activeUser = $filter('find')($scope.users, {
               id: $routeParams.id
-            })[0];
+            });
 
             $scope.selectedUser = activeUser;
           }
