6f8e0176d87e79f60d47a2b558c79386942fbe9b
Matthew Morgan <hexfury@gmail.com>
2015-06-01 15:19:25 -0700

    default to localhost:9800

--- resources/wrapper.sh
+++ resources/wrapper.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-show_usage () {
-  printf "API Hostname Required: $1 \n"
-  printf "Please pass in the api hostname as an argument, ex. http://localhost:9800.\n"
-}
-
-if [ -z "$1" ]
+if [ -n "$1" ]
   then
-    show_usage
-    exit 1
-  else
-    sed -ri "s|http://localhost:9080|$1|g" /usr/share/nginx/html/app/env.js
-    printf "Contents of env.js:\n"
-    cat /usr/share/nginx/html/app/env.js
-    printf "\nNginx is running... Waiting for requests...\n"
-    nginx -g "daemon off;"
+  sed -ri "s|http://localhost:9080|$1|g" /usr/share/nginx/html/app/env.js
+  printf "Contents of env.js:\n"
+  cat /usr/share/nginx/html/app/env.js
 fi
+
+printf "\nNginx is running... Waiting for requests...\n"
+nginx -g "daemon off;"
