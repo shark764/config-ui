085afa0655c6606e6a6eb0e1a7360582d0c80c3e
Greg Royan <greg.royan@bluespurs.com>
2015-06-03 15:02:45 -0300

    TITAN-1675 - Updated to use the session value for tenantId for create.  Displays different flows depending on the selected tenant.  Need tenant change to bubble down to children for refresh.

--- src/app/components/flows/flowsController.js
+++ src/app/components/flows/flowsController.js
@@ -9,60 +9,42 @@ angular.module('liveopsConfigPanel')
       $scope.flows = [];
       $scope.tenants = [];
 
+      $scope.$on('$routeUpdate', function () {
+        $scope.setFlow($routeParams.id);
+      });
+
       RegionsService.query(function (data){
         $scope.regions = data.result;
         $scope.fetch($scope.regions[0].id);
       });
 
-      $scope.setTenant = function (id) {
-        var activeTenant = $filter('filter')($scope.tenants, {id : id})[0];
-        $scope.tenant = id ? activeTenant : {  } ;
+      $scope.setFlow = function (id) {
+        var activeFlow = $filter('filter')($scope.flows.result, {id : id})[0];
+        $scope.flow = id ? activeFlow : {  } ;
       };
 
       $scope.fetch = function (regionId) {
-        TenantsService.query( { regionId : regionId }, function (data) {
-          $scope.tenants = data.result;
-          $scope.setTenant($routeParams.id);
-        });
-
-        FlowsService.query( { tenantId: Session.tenantId }, function (data){
-          console.log(Session);
-         console.log(data);
-         $scope.flows = data.result;
-       });
+        $scope.flows = FlowsService.query( { tenantId: Session.tenantId });
       };
 
-      $scope.getTenantName = function (tenantId) {
-        var tenantName = tenantId;
-        // TenantsService.query( { id: tenantId }, function (data){
-        //   tenantName = data.result.name;
-        // });
-        return tenantName;
-      }
-
       $scope.saveSuccess = function (response) {
-        console.log("success");
-        console.log(response);
-
         $scope.flow = {};
         $scope.fetch($scope.regions[0].id);
       };
 
       $scope.saveFailure = function (reason) {
-        console.log("error");
-        console.log(reason);
         $scope.error = reason.data;
       };
 
       $scope.save = function () {
         if(!$scope.flow.id){
-          return FlowsService.save({ tenantId : $scope.flow.tenantId }, $scope.flow).$promise
+          return FlowsService.save({ tenantId : Session.tenantId }, $scope.flow).$promise
           .then(
             $scope.saveSuccess,
             $scope.saveFailure
             );
         } else {
-          return FlowsService.update({ tenantId: $scope.flow.tenantId, flowId : $scope.flow.id }, $scope.flow).$promise
+          return FlowsService.update({ tenantId: Session.tenantId, flowId : $scope.flow.id }, $scope.flow).$promise
           .then(
             $scope.saveSuccess,
             $scope.saveFailure
--- src/app/components/flows/flowsService.js
+++ src/app/components/flows/flowsService.js
@@ -3,6 +3,6 @@
 angular.module('liveopsConfigPanel')
   .factory('FlowsService', ['ServiceFactory', function (ServiceFactory) {
 
-    return ServiceFactory.create('/v1/tenants/:tenantId/flows', true, false);
+    return ServiceFactory.create('/v1/tenants/:tenantId/flows/:flowId', true, false);
   }]);
 
