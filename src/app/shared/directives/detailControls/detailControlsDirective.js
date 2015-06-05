5f2c5f211d6f551166cf1e3c03f182dcc4980861
John Benson <john.benson@bluespurs.com>
2015-06-05 14:52:27 -0300

    Starting details controls.

---
+++ src/app/shared/directives/detailControls/detailControlsDirective.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('liveopsConfigPanel')
+  .directive('detailControls', [function() {
+    return {
+      restrict: 'E',
+      scope : {
+        resource: '=',
+        form: '='
+      },
+      templateUrl : 'app/shared/directives/detailControls/detailControls.html',
+      link : function($scope) {
+        $scope.oResource = $scope.resource;
+
+        $scope.$watch('resource', function () {
+          $scope.oResource = angular.copy($scope.resource);
+        });
+
+        $scope.cancel = function () {
+          $scope.resource = angular.copy($scope.oResource);
+          $scope.form.$setPristine();
+        };
+      }
+    };
+   }]);
