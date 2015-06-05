03cef0615afbb5c6d94a78afd24e32de33396ec3
John Benson <john.benson@bluespurs.com>
2015-06-05 11:48:29 -0300

    Moved the user table into the users controller

--- src/app/components/users/usersController.js
+++ src/app/components/users/usersController.js
@@ -1,4 +1,49 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .controller('UsersController', function () {});
+  .controller('UsersController', ['$scope', '$location', '$routeParams', '$filter', 'userStates', 'userStatuses', 'columns', 'User',
+      function ($scope, $location, $routeParams, $filter, userStates, userStatuses, columns, User) {
+    $scope.states = userStates;
+    $scope.statuses = userStatuses;
+    $scope.columns = columns;
+    $scope.filteredUsers = [];
+
+    $scope.users = User.query(function () {
+
+      if($routeParams.id) {
+        var activeUser = $filter('filter')($scope.users, {
+          id: $routeParams.id
+        }, true);
+
+        $scope.selectedUser = activeUser;
+      }
+    });
+
+    $scope.$on('user:cancel', function () {
+      //TODO: undo changes to scope selectedUser without a query
+      $scope.selectedUser.$get({id: $scope.selectedUser.id});
+    });
+
+    $scope.$watchCollection(function(){ return $scope.filteredUsers;}, function(newList){
+      //Only replace the selected user if the old one got excluded via filtering.
+      if (newList && newList.indexOf($scope.selectedUser) === -1){
+        $scope.selectedUser = $scope.filteredUsers[0];
+      }
+    });
+
+    $scope.selectUser = function (user) {
+      $scope.selectedUser = user;
+      $location.search({
+        id: user.id
+      });
+    };
+
+    $scope.createUser = function () {
+      $scope.$broadcast('user:create');
+    };
+
+    $scope.$on('user:created', function (event, user) {
+      $scope.users.push(user);
+    });
+
+  }]);
--- src/app/components/users/usersController.spec.js
+++ src/app/components/users/usersController.spec.js
@@ -1,18 +1,67 @@
 'use strict';
 
 describe('UsersController', function() {
-    var $scope,
-        $controller,
-        controller;
+  var $scope,
+    $httpBackend,
+    element,
+    users;
 
-    beforeEach(module('liveopsConfigPanel'));
-    beforeEach(inject(['$rootScope', '$controller', function($rootScope, _$controller_) {
-      $scope = $rootScope.$new();
-      $controller = _$controller_;
-      controller = $controller('UsersController', {'$scope': $scope});
-    }]));
-    
-    it('should exist', function(){
-      expect(controller).toBeDefined();
-    });
+  beforeEach(module('liveopsConfigPanel'));
+  beforeEach(module('gulpAngular'));
+
+  beforeEach(inject(['$compile', '$rootScope', '$injector', 'apiHostname', function($compile, $rootScope, $injector, apiHostname) {
+    users  = [ {
+      'id': 'c6aa44f6-b19e-49f5-bd3f-66f00b885e39',
+      'status': false,
+      'externalId': 73795,
+      'state': 'WRAP',
+      'lastName': 'Lowe',
+      'firstName': 'Munoz',
+      'email': 'munoz.lowe@hivedom.org',
+      'displayName': 'Munoz Lowe'
+    },
+    {
+      'id': '9f97f9d9-004c-469c-906d-b917bd79fbe8',
+      'status': true,
+      'externalId': 80232,
+      'state': 'NOT_READY',
+      'lastName': 'Oliver',
+      'firstName': 'Michael',
+      'email': 'michael.oliver@ezent.io',
+      'displayName': 'Michael Oliver'
+    }];
+
+    //Need the following so that $digest() works
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', apiHostname + '/v1/users').respond({'result' : users});
+    $httpBackend.expectGET(apiHostname + '/v1/users');
+
+    $scope = $rootScope.$new();
+    $scope.users = users;
+    element = $compile('<user-table></user-table>')($scope);
+    $scope.$digest();
+    $httpBackend.flush();
+  }]));
+
+  it('should have users', inject(function() {
+    expect($scope.users).toBeDefined();
+    expect($scope.users.length).toEqual(2);
+  }));
+
+  it('should fetch initial list of users', inject(function() {
+    expect($scope.users).toBeDefined();
+    expect($scope.users.length).toEqual(users.length);
+  }));
+
+  it('should insert a row for each user, plus a header row', inject(function() {
+    expect(element.find('tr').length).toEqual(users.length + 1);
+  }));
+
+  it('should have statuses and states objects', inject(function() {
+    expect($scope.statuses).toBeDefined();
+    expect($scope.statuses).toEqual(jasmine.any(Object));
+
+    expect($scope.states).toBeDefined();
+    expect($scope.states).toEqual(jasmine.any(Object));
+  }));
 });
