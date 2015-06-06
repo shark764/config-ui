4ad21c86c26db5baadace65a964c124833efba17
John Benson <johnedwardbenson@gmail.com>
2015-06-06 14:42:35 -0300

    Created directive to easily have reproduce "details" sections.

--- src/app/shared/auth/session.js
+++ src/app/shared/auth/session.js
@@ -11,8 +11,8 @@
 
 // this will suffice in beta however.
 angular.module('liveopsConfigPanel')
-  .service('Session', ['$rootScope', 'sessionKey', 'preferenceKey', '$translate',
-    function ($rootScope, sessionKey, preferenceKey, $translate) {
+  .service('Session', ['$rootScope', 'sessionKey', 'preferenceKey', '$injector',
+    function ($rootScope, sessionKey, preferenceKey, $injector) {
       var self = this;
 
       this.userSessionKey = sessionKey;
@@ -74,7 +74,6 @@ angular.module('liveopsConfigPanel')
         angular.extend(this, JSON.parse(localStorage.getItem(this.userPreferenceKey)));
 
         if (this.lang) {
-          $translate.use(this.lang);
         }
       };
 
---
+++ src/app/shared/directives/resourceDetails/resourceDirective.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('liveopsConfigPanel')
+  .directive('resourceDetails', [function() {
+    return {
+      restrict: 'E',
+      scope : {
+        resource: '=',
+        headerTemplateUrl: '@',
+        bodyTemplateUrl: '@',
+        extendScope: '='
+      },
+      templateUrl : 'app/shared/directives/resourceDetails/resourceDetails.html',
+
+      link : function($scope) {
+
+        angular.extend($scope, $scope.extendScope);
+        $scope.oResource = angular.copy($scope.resource);
+
+        $scope.save = function () {
+          $scope.resource.save({id: $scope.resource.id},
+            function (result) {
+              $scope.detailsForm.$setPristine();
+              $scope.resource = result;
+              $scope.oResource = angular.copy($scope.resource);
+            }
+          );
+        };
+
+        $scope.$watch('resource', function () {
+          $scope.oResource = angular.copy($scope.resource);
+        });
+
+        $scope.cancel = function () {
+          angular.copy($scope.oResource, $scope.resource);
+          $scope.detailsForm.$setPristine();
+        };
+      }
+    };
+   }]);
