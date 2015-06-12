01581cf34777ef7e0e84a5ab0bbae5b31713891d
John Benson <john.benson@bluespurs.com>
2015-06-12 16:15:00 -0300

    Groups and skills fixed

--- src/app/components/management/groups/groupsController.js
+++ src/app/components/management/groups/groupsController.js
@@ -20,17 +20,6 @@ angular.module('liveopsConfigPanel')
         $scope.fetch();
       });
 
-      //dummy data until we have members
-      var users = User.query();
-
-      $scope.groups.$promise.then(function() {
-        users.$promise.then(function(){
-          angular.forEach($scope.groups, function(group) {
-            group.members = users;
-          });
-        });
-      });
-
       $scope.createGroup = function() {
         $scope.selectedGroup = new Group({
           tenantId: Session.tenant.tenantId,
