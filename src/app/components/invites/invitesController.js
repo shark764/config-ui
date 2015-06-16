f22ead61b34c530eb9d13fd826d7a4bc50d238bd
John Benson <john.benson@bluespurs.com>
2015-06-16 11:19:42 -0300

    re-adding invite sending portion to invites screen.

--- src/app/components/invites/invitesController.js
+++ src/app/components/invites/invitesController.js
@@ -11,21 +11,27 @@ angular.module('liveopsConfigPanel')
       $scope.init = function(){
         $scope.newInvite = new Invite();
         $scope.newInvite.roleId = '10f15d80-0052-11e5-b68b-fb65b1fe22e1'; //TEMPORARY until roles are implemented
+        $scope.newInvite.tenantId = $scope.tenants[0].id;
       };
-
-      $scope.init();
 
       $scope.tenants = Tenant.query({regionId : Session.activeRegionId}, function () {
         if($scope.tenants.length > 0){
           $scope.fetchInvites();
+          $scope.init();
         }
       });
 
-      $scope.$watch('newInvite.tenantId', function () {
-        if($scope.newInvite.tenantId){
+      $scope.save = function(){
+        var prevTenant = $scope.newInvite.tenantId;
+
+        $scope.newInvite.save({tenantId : $scope.newInvite.tenantId}, function(data){
+          $scope.init();
+          $scope.newInvite.tenantId = prevTenant;
           $scope.fetchInvites();
-        }
-      });
+        }, function () {
+          toastr.error('Failed to create invite');
+        });
+      };
 
       $scope.remove = function(invite){
         invite.$delete({tenantId: invite.tenantId, userId: invite.userId, token: invite.invitationToken}, function(){
