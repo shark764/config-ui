0eddca51055a10dad82b0e9110b5b6cdf7b1288b
John Benson <john.benson@bluespurs.com>
2015-06-19 10:15:17 -0300

    Fix JSHint issue

--- src/app/components/management/users/usersController.js
+++ src/app/components/management/users/usersController.js
@@ -61,7 +61,7 @@ angular.module('liveopsConfigPanel')
       };
 
       $scope.fetch = function () {
-        $scope.users = User.query({tenantId : Session.tenant.tenantId != '' ? Session.tenant.tenantId : null });
+        $scope.users = User.query({tenantId : Session.tenant.tenantId !== '' ? Session.tenant.tenantId : null });
       };
 
       $scope.fetch();
