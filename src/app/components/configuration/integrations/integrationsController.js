83ac0ea921bdfabc32cc21251e89d6bcf629b17a
Phil Hachey <philywily123@gmail.com>
2015-06-19 14:19:09 -0300

    Added watch for Session.tenant

--- src/app/components/configuration/integrations/integrationsController.js
+++ src/app/components/configuration/integrations/integrationsController.js
@@ -3,16 +3,21 @@
 angular.module('liveopsConfigPanel')
   .controller('IntegrationsController', ['$scope', '$state', 'Session', 'Integration', 'integrationTableConfig',
     function($scope, $state, Session, Integration, integrationTableConfig) {
-      $scope.integrations = Integration.query({
-        tenantId: Session.tenant.tenantId
-      });
-      
+
+      $scope.fetch = function() {
+        $scope.integrations = Integration.query({
+          tenantId: Session.tenant.tenantId
+        });
+      };
+
+      $scope.$watch('Session.tenant', $scope.fetch);
+
       $scope.$on('on:click:create', function() {
         $scope.selectedIntegration = new Integration({
           tenantId: Session.tenant.tenantId
         });
       });
-      
+
       $scope.tableConfig = integrationTableConfig;
     }
   ]);
