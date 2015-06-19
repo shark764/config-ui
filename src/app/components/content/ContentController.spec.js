3e4a842697a8c206c32e2cadd620157aac282848
Sasha Wilcox <sasha.wilcox@bluespurs.com>
2015-06-19 15:25:13 -0300

    Changing test name to match file name

--- src/app/components/content/ContentController.spec.js
+++ src/app/components/content/ContentController.spec.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-describe('ConfigurationController', function () {
+describe('ContentController', function () {
   var $scope,
     $controller,
     $httpBackend,
@@ -25,7 +25,7 @@ describe('ConfigurationController', function () {
       apiHostname = _apiHostname_;
     }
   ]));
-  
+
   beforeEach(function() {
     regions = [{
       'id': 'c98f5fc0-f91a-11e4-a64e-7f6e9992be1f',
@@ -34,34 +34,34 @@ describe('ConfigurationController', function () {
     }];
 
     tenantId = 'c98f5fc0-f91a-11e4-a64e-000e9992be1f';
-    
+
     $httpBackend.when('GET', apiHostname + '/v1/regions').respond({'result' : regions});
-    
+
     $controller('ContentController', {'$scope': $scope});
-    
+
     $httpBackend.flush();
   });
-  
+
   it('should set activeRegionId to first region found', function () {
     expect($scope.regions[0].id).toEqual(regions[0].id);
     expect(Session.activeRegionId).toEqual(regions[0].id);
   });
-  
+
   it('should transition when Session tenantId is not set', function () {
     spyOn($state, 'transitionTo');
-    
+
     $scope.redirectToInvites();
-    
+
     expect($state.transitionTo).toHaveBeenCalled();
   });
-  
+
   it('should not transition when Session tenantId is set', function () {
     Session.tenant.tenantId = tenantId;
-    
+
     spyOn($state, 'transitionTo');
-    
+
     $scope.redirectToInvites();
-    
+
     expect($state.transitionTo).not.toHaveBeenCalled();
   });
 });
