851e0f6e558370b06d4554d45ce8e2c8053d8835
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-06-22 16:56:19 -0300

    Subflow stuff most of the way there. Need drafts possibly...

--- src/app/components/designer/designer/designerDirective.js
+++ src/app/components/designer/designer/designerDirective.js
@@ -9,10 +9,29 @@ function flowDesigner() {
       templateUrl: 'app/components/designer/designer/designerDirective.html',
       replace: true,
       link: function() {},
-      controller: ['$scope', '$element', '$attrs', '$window', '$timeout', 'JointInitService', 'FlowConversionService', 'FlowNotationService', 'FlowVersion', 'Session', 'toastr', function($scope, $element, $attrs, $window, $timeout, JointInitService, FlowConversionService, FlowNotationService, FlowVersion, Session, toastr) {
+      controller: ['$scope', '$element', '$attrs', '$window', '$timeout', 'JointInitService', 'FlowConversionService', 'FlowNotationService', 'FlowVersion', 'Session', 'toastr', '$state', function($scope, $element, $attrs, $window, $timeout, JointInitService, FlowConversionService, FlowNotationService, FlowVersion, Session, toastr, $state) {
 
         $timeout(function() {
           var graph = JointInitService.graph(1280, 800, 20, true, true, false, new joint.shapes.liveOps.link(), 0, true, [], '#stencil-container', '#paper-container', '#inspector-container');
+
+          graph.interfaces.paper.on({
+            'cell:pointerdblclick': function(cellView) {
+              if (cellView.model.attributes.name !== 'subflow') { return; }
+              console.log('DOUBLE CLICKED', cellView);
+              $scope.redirectToSubflowEditor(cellView);
+            }
+          });
+
+          $scope.redirectToSubflowEditor = function(cellView) {
+            console.log(cellView);
+            $state.go('content.designer.subflowEditor', {
+              parentName: $scope.flowVersion.name,
+              notationName: cellView.model.attributes.params.resource || 'N/A',
+              parentFlowId: $scope.flowVersion.flowId,
+              parentVersionId: $scope.flowVersion.version,
+              subflowNotationId: cellView.model.id
+            });
+          };
 
           $scope.publishNewFlowVersion = function() {
             if (graph.toJSON().cells.length === 0) { return; }
--- src/app/components/designer/subflow/subflowDesignerDirective.js
+++ src/app/components/designer/subflow/subflowDesignerDirective.js
@@ -9,7 +9,7 @@ function subflowDesigner() {
       templateUrl: 'app/components/designer/subflow/subflowDesignerDirective.html',
       replace: true,
       link: function() {},
-      controller: ['$scope', '$element', '$attrs', '$window', '$timeout', 'JointInitService', 'SubflowCommunicationService', function($scope, $element, $attrs, $window, $timeout, JointInitService, SubflowCommunicationService) {
+      controller: ['$scope', '$element', '$attrs', '$window', '$timeout', 'JointInitService', 'SubflowCommunicationService', '$state',function($scope, $element, $attrs, $window, $timeout, JointInitService, SubflowCommunicationService, $state) {
         console.log('SF in directive:', $scope.subflow);
         $timeout(function() {
           var graph = JointInitService.graph(1280, 800, 20, true, true, false, new joint.shapes.liveOps.link(), 0, true, [], '#stencil-container', '#paper-container', '#inspector-container');
@@ -23,7 +23,11 @@ function subflowDesigner() {
               parentVersionId: $scope.subflow.parentVersionId
             });
 
-            console.log(SubflowCommunicationService);
+            $state.go('content.designer.editor', {
+              flowId: $scope.subflow.parentFlowId,
+              versionId: $scope.subflow.parentVersionId,
+              version: 'v99'
+            });
           };
 
           if ($scope.subflow.graphJSON !== '{"cells":[]}') {
