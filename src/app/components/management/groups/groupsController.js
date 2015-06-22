4077413da9a9caa5c03bd075de143f4eaefb3a3d
Phil Hachey <phil.hachey@bluespurs.com>
2015-06-22 14:08:11 -0300

    Fixed groupController tests

--- src/app/components/management/groups/groupsController.js
+++ src/app/components/management/groups/groupsController.js
@@ -2,39 +2,46 @@
 
 angular.module('liveopsConfigPanel')
   .controller('GroupsController', ['$scope', 'Session', 'Group', 'User', 'groupTableConfig', 'TenantGroupUsers', 'UserName',
-    function($scope, Session, Group, User, groupTableConfig, TenantGroupUsers, UserName) {
+    function ($scope, Session, Group, User, groupTableConfig, TenantGroupUsers, UserName) {
       $scope.Session = Session;
 
       $scope.tableConfig = groupTableConfig;
 
-      $scope.$watch('Session.tenant.tenantId', $scope.fetch, true);
-
       //This is really awful and hopefully the API will update to accommodate this.
       $scope.fetch = function () {
-        $scope.groups = Group.query({tenantId: Session.tenant.tenantId}, function(){
-          angular.forEach($scope.groups, function(item, itemKey){
+        $scope.groups = Group.query({
+          tenantId: Session.tenant.tenantId
+        }, function () {
+          angular.forEach($scope.groups, function (item, itemKey) {
             $scope.updateMembers($scope.groups[itemKey]);
           });
         });
       };
 
-      $scope.additional = {
-          postSave : function(childScope){
-            $scope.updateMembers(childScope.originalResource);
-          }
-      };
-
-      $scope.updateMembers = function(group){
-        group.members = TenantGroupUsers.query({tenantId: Session.tenant.tenantId, groupId: group.id}, function(){
-          angular.forEach(group.members, function(member, key){
-            UserName.get(member.memberId, function(data){
+      $scope.updateMembers = function (group) {
+        group.members = TenantGroupUsers.query({
+          tenantId: Session.tenant.tenantId,
+          groupId: group.id
+        }, function () {
+          angular.forEach(group.members, function (member, key) {
+            UserName.get(member.memberId, function (data) {
               group.members[key].displayName = data.displayName;
             });
           });
         });
       };
 
-      $scope.$on('on:click:create', function(){
+      $scope.additional = {
+        postSave: function (childScope) {
+          $scope.updateMembers(childScope.originalResource);
+        }
+      };
+
+      $scope.$watch('Session.tenant', function() {
+        $scope.fetch();
+      }, true);
+
+      $scope.$on('on:click:create', function () {
         $scope.selectedGroup = new Group({
           tenantId: Session.tenant.tenantId,
           status: true,
