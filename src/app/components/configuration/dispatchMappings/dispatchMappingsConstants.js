f0f170f9e7250a651974ee03dad3a11d5d4fe381
Phil Hachey <philywily123@gmail.com>
2015-06-22 21:26:21 -0300

    TITAN2-1065 updated dispatch mappings dropdown mappings

--- src/app/components/configuration/dispatchMappings/dispatchMappingsConstants.js
+++ src/app/components/configuration/dispatchMappings/dispatchMappingsConstants.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('liveopsConfigPanel.config')
-  .constant('dispatchMappingInteractionTypes', [{
+  .constant('dispatchMappingChannelTypes', [{
+    display: 'Any',
+    value: 'any'
+  }, {
     display: 'Voice',
     value: 'voice'
   }, {
@@ -13,9 +16,26 @@ angular.module('liveopsConfigPanel.config')
   }, {
     display: 'Chat',
     value: 'chat'
+  }, {
+    display: 'Facebook',
+    value: 'facebook'
+  }, {
+    display: 'Twitter',
+    value: 'twitter'
+  }, {
+    display: 'etc',
+    value: 'etc'
   }])
-  .constant('dispatchMappingChannelTypes', [{
+  .constant('dispatchMappingInteractionFields', [{
+    display: 'Customer',
+    value: 'customer'
+  }, {
     display: 'Contact Point',
-    value: 'contactPoint'
+    value: 'contact-poing'
+  }, {
+    display: 'Integration',
+    value: 'integration'
+  }, {
+    display: 'Direction',
+    value: 'direction'
   }]);
-  
--- src/app/components/configuration/dispatchMappings/dispatchMappingsController.js
+++ src/app/components/configuration/dispatchMappings/dispatchMappingsController.js
@@ -2,32 +2,44 @@
 
 angular.module('liveopsConfigPanel')
   .controller('DispatchMappingsController', [
-    '$scope', 'Session', 'DispatchMapping', 'Flow', 'dispatchMappingTableConfig', 'dispatchMappingInteractionTypes', 'dispatchMappingChannelTypes',
-    function($scope, Session, DispatchMapping, Flow, dispatchMappingTableConfig, dispatchMappingInteractionTypes, dispatchMappingChannelTypes) {
-      $scope.fetch = function() {
-        $scope.dispatchMappings = DispatchMapping.query({
+    '$scope', 'Session', 'DispatchMapping', 'Flow', 'dispatchMappingTableConfig', 'dispatchMappingInteractionFields', 'dispatchMappingChannelTypes',
+    function($scope, Session, DispatchMapping, Flow, dispatchMappingTableConfig, dispatchMappingInteractionFields, dispatchMappingChannelTypes) {
+      $scope.create = function() {
+        $scope.selectedDispatchMapping = new DispatchMapping({
           tenantId: Session.tenant.tenantId
         });
-        
+      };
+
+      $scope.fetch = function() {
+        if (!Session.tenant || !Session.tenant.tenantId) {
+          return;
+        }
+
+        $scope.dispatchMappings = DispatchMapping.query({
+          tenantId: Session.tenant.tenantId
+        }, function() {
+          if (!$scope.dispatchMappings.length) {
+            $scope.create();
+          }
+        });
+
         $scope.flows = Flow.query({
           tenantId: Session.tenant.tenantId
         });
       };
-      
+
       $scope.$watch('Session.tenant.tenantId', $scope.fetch, true);
 
       $scope.$on('on:click:create', function() {
-        $scope.selectedDispatchMapping = new DispatchMapping({
-          tenantId: Session.tenant.tenantId
-        });
+        $scope.create();
       });
-      
+
       $scope.tableConfig = dispatchMappingTableConfig;
       $scope.fetch();
-      
+
       $scope.additional = {
         flows: $scope.flows,
-        dispatchMappingInteractionTypes: dispatchMappingInteractionTypes,
+        dispatchMappingInteractionFields: dispatchMappingInteractionFields,
         dispatchMappingChannelTypes: dispatchMappingChannelTypes
       };
     }
