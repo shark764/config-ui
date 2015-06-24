680f3ecba13cbee85dd17aa5e47c5b8b55b7f598
Nick Fitzpatrick <nickfitz.582@gmail.com>
2015-06-24 16:19:35 -0300

    reenabeled multiple boundry events

--- src/app/components/designer/designer/notations/joint.shapes.liveOps.activity.js
+++ src/app/components/designer/designer/notations/joint.shapes.liveOps.activity.js
@@ -121,13 +121,34 @@
         cell.set('z', 2);
         var box = cell.getBBox();
         joint.util.nextFrame(function() {
-          _.forEach(cell.getEmbeddedCells(), function(child) {
-            if (!child) { return; }
-            child.set('position',
-            {
-              x: (box.x + box.width) - 15,
-              y: (box.y + box.height) - 15
-            });
+          _.forEach(cell.getEmbeddedCells(), function(child, index) {
+            if (!child) {return;}
+
+            if (index == 0) {
+              child.set('position',
+              {
+                x: (box.x + box.width) - 15,
+                y: (box.y + box.height) - 15
+              });
+            }else if (index == 1) {
+              child.set('position',
+              {
+                x: (box.x + box.width) - 15,
+                y: (box.y) - 15
+              });
+            } else if (index == 2) {
+              child.set('position',
+              {
+                x: (box.x) - 15,
+                y: (box.y + box.height) - 15
+              });
+            } else if (index == 3) {
+              child.set('position',
+              {
+                x: (box.x) - 15,
+                y: (box.y) - 15
+              });
+            }
           });
         });
 
