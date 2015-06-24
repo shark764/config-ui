f885623fdb86bd045942185ed8f99d78ca81113f
Phil Hachey <phil.hachey@bluespurs.com>
2015-06-24 10:27:30 -0300

    TITAN2-1282 fixed media colleciton list not updating and added constant for mediaTypes

--- src/app/components/designer/media/mediaConstants.js
+++ src/app/components/designer/media/mediaConstants.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
+<<<<<<< HEAD
   .factory('mediaTypes', ['$filter',  function($filter) {
     return [{
       display: $filter('translate')('media.details.mediaTypes.audio'),
@@ -9,4 +10,12 @@ angular.module('liveopsConfigPanel')
       display: $filter('translate')('media.details.mediaTypes.tts'),
       value: 'tts'
     }]
+=======
+  .constant('mediaTypes', [{
+    display: 'Audio',
+    value: 'audio'
+  }, {
+    display: 'TTS',
+    value: 'tts'
+>>>>>>> TITAN2-1282 fixed media colleciton list not updating and added constant for mediaTypes
   }]);
