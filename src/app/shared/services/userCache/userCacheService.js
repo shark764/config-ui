312eff2029f4a1479678c1ca413b05f980080f7e
John Benson <john.benson@bluespurs.com>
2015-07-03 16:25:01 -0300

    User cache now adding tenant ID.

--- src/app/shared/services/userCache/userCacheService.js
+++ src/app/shared/services/userCache/userCacheService.js
@@ -1,42 +1,42 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .service('UserCache', ['$q', 'User', 'UUIDCache', function ($q, User, UUIDCache) {
-    this.get = function (id, success, failure) {
-      var deferred = $q.defer();
-      if (id) {
-        var cached = UUIDCache.get(id);
-        if (cached) {
-          deferred.resolve(cached);
-          
-          if(success) {
-            success(cached);
-          }
-          
-          return angular.extend({
-            $promise: deferred.promise,
-          }, cached);
-        } else {
-          return User.get({
-            id: id
-          }, function (user) {
-            if(success) {
-              success(user);
-            }
-            
-            UUIDCache.put(id, {
-              id: user.id,
-              displayName: user.displayName
-            });
-          }, function(error) {
-            failure(error);
-          });
-        }
-      }
+  .service('UserCache', ['$q', 'User', 'UUIDCache', 'Session',
+    function ($q, User, UUIDCache, Session) {
+      this.get = function (id, success, failure) {
+        var deferred = $q.defer();
+        if (id) {
+          var cached = UUIDCache.get(id);
+          if (cached) {
+            deferred.resolve(cached);
 
-      deferred.reject('id_not_set');
-      return {
-        $promise: deferred.promise
+            if(success) {
+              success(cached);
+            }
+
+            return angular.extend({
+              $promise: deferred.promise,
+            }, cached);
+          } else {
+            return User.get({
+              id: id,
+              tenantId: Session.tenant.tenantId
+            }, function (user) {
+              success ? success(user) : angular.noop;
+
+              UUIDCache.put(id, {
+                id: user.id,
+                displayName: user.displayName
+              });
+            }, function(error) {
+              failure ? failure(error) : angular.noop;
+            });
+          }
+        }
+
+        deferred.reject('id_not_set');
+        return {
+          $promise: deferred.promise
+        };
       };
-    };
-  }]);
+    }]);
