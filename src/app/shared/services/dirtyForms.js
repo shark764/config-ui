de993f1d62ec0a9a4036b48b0634edd5cc24517c
John Benson <johnedwardbenson@gmail.com>
2015-07-06 15:30:26 -0300

    Swapped cancel and OK options for confirmation so that it will select the correct one if alerts are disabled by first user.

--- src/app/shared/services/dirtyForms.js
+++ src/app/shared/services/dirtyForms.js
@@ -4,7 +4,7 @@ angular.module('liveopsConfigPanel')
   .service('DirtyForms', ['Alert', '$translate', function (Alert, $translate) {
     var self = this;
     this.forms = [];
-    
+
     this.hasDirty = function(){
       var hasDirty = false;
       angular.forEach(self.forms, function(form){
@@ -12,23 +12,23 @@ angular.module('liveopsConfigPanel')
           hasDirty = true;
         }
       });
-      
+
       return hasDirty;
     };
-    
+
     this.registerForm = function(form){
       self.forms.push(form);
     };
-    
+
     this.removeForm = function(form){
       self.forms.removeItem(form);
     };
-    
+
     this.confirmIfDirty = function(actionFunction){
       if (self.hasDirty()){
         Alert.confirm($translate.instant('dirtyforms.confirm.warning'),
-          actionFunction,
-          angular.noop);
+          angular.noop,
+          actionFunction);
       } else {
         actionFunction();
       }
