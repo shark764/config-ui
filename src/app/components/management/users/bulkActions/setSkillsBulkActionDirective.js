573d1e47b111c4a8d062593976c82a7fb47c2d35
Greg Royan <greg.royan@bluespurs.com>
2015-07-08 15:13:27 -0300

    TITAN2-1606 - Added skills add, update, delete bulk actions.

--- src/app/components/management/users/bulkActions/setSkillsBulkActionDirective.js
+++ src/app/components/management/users/bulkActions/setSkillsBulkActionDirective.js
@@ -1,55 +1,78 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .directive('baSetSkills', [ 'Skill', 'Session',
-    function (Skill, Session) {
-      return {
-        restrict: 'AE',
-        scope: {
-          bulkAction: '='
-        },
-        templateUrl: 'app/components/management/users/bulkActions/setSkillsBulkAction.html',
-        link: function ($scope) {
-          $scope.skillActions = [
-            {
-              action: 'add',
-              label:'Add Skill'
-            },
-            {
-              action: 'update',
-              label:'Update Skill'
-            },
-            {
-              action: 'remove',
-              label:'Remove Skill'
-            }
-          ];
-          var countSkills = 0;
-          $scope.skillsToAdd = [];
+.directive('baUserSkills', [ '$q', 'userSkillsBulkActionTypes', 'Skill', 'Session', '$translate', 'TenantUserSkills',
+  function ($q, userSkillsBulkActionTypes, Skill, Session, $translate, TenantUserSkills) {
+    return {
+      restrict: 'AE',
+      scope: {
+        bulkAction: '='
+      },
+      templateUrl: 'app/components/management/users/bulkActions/setSkillsBulkAction.html',
+      link: function ($scope) {
+        $scope.userSkillsBulkActionTypes = userSkillsBulkActionTypes;
+        var countSkills = 0;
+        $scope.userSkillsBulkActions = [];
 
-          $scope.skills = Skill.query({
-            tenantId: Session.tenant.tenantId
+        $scope.skills = Skill.query({
+          tenantId: Session.tenant.tenantId
+        });
+
+        $scope.removeBulkSkill = function(item) {
+          var index = $scope.userSkillsBulkActions.indexOf(item);
+          $scope.userSkillsBulkActions.splice(index, 1);     
+        }
+
+        $scope.addBulkSkill = function() {
+          $scope.userSkillsBulkActions.push({id: countSkills});
+          countSkills++;
+        }
+
+        $scope.bulkAction.action = function (user) {
+          var promises = [];
+          angular.forEach($scope.userSkillsBulkActions, function(userSkillsBulkAction) {
+            if(!userSkillsBulkAction.selectedSkill || !userSkillsBulkAction.selectedType) {
+              return;
+            }
+
+            if(userSkillsBulkAction.selectedType === 'add') {
+              var tenantUserSkill = new TenantUserSkills();
+
+              tenantUserSkill.skillId = userSkillsBulkAction.selectedSkill.id;
+              tenantUserSkill.proficiency = userSkillsBulkAction.selectedSkill.proficiency;
+
+              promises.push(tenantUserSkill.$create({
+                tenantId: Session.tenant.tenantId,
+                userId: user.id
+              }));
+
+            } else if (userSkillsBulkAction.selectedType === 'remove') {
+
+              var tenantUserSkill = new TenantUserSkills();
+
+              promises.push(tenantUserSkill.$delete({
+                skillId: userSkillsBulkAction.selectedSkill.id,
+                tenantId: Session.tenant.tenantId,
+                userId: user.id
+              }));
+
+            } else if (userSkillsBulkAction.selectedType ==='update') {
+
+              var tenantUserSkill = new TenantUserSkills();
+              tenantUserSkill.proficiency = userSkillsBulkAction.selectedSkill.proficiency;
+              tenantUserSkill.userId = user.id;
+              tenantUserSkill.tenantId = Session.tenant.tenantId;
+
+              promises.push(tenantUserSkill.$update({
+                skillId: userSkillsBulkAction.selectedSkill.id
+              }));
+
+            }
           });
 
-          $scope.removeBulkSkill = function(item) {
-            var index = $scope.skillsToAdd.indexOf(item);
-            $scope.skillsToAdd.splice(index, 1);     
-          }
-
-          $scope.addBulkSkill = function() {
-            $scope.skillsToAdd.push({id: countSkills});
-            countSkills++;
-
-            console.log($scope.skillsToAdd);
-          }
-
-          $scope.bulkAction.action = function(user) {
-            console.log("Herro?");
-            //user.skills = $scope.skills;
-            console.log($scope.skillsToAdd);
-            return user;
-          }
-        }
-      };
-    }
-  ]);
+          return $q.all(promises);
+        };
+      }
+    };
+  }
+]);
