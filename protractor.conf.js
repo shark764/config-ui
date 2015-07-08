61f54788d5fe8ccaad613f2973651d211e97de4d
Sasha Wilcox <sasha.wilcox@bluespurs.com>
2015-07-08 12:17:27 -0300

    Merging e2e-queues

--- protractor.conf.js
+++ protractor.conf.js
@@ -46,16 +46,17 @@ exports.config = {
   },
 
   specs: [
-    paths.e2e + '/login/login.spec.js',
-    paths.e2e + '/navigation/sidebar.spec.js',
-    paths.e2e + '/tableControls/**/*.spec.js',
-    paths.e2e + '/management/**/*.spec.js',
-    paths.e2e + '/configuration/**/*.spec.js',
-    paths.e2e + '/userProfile/**/*.spec.js',
-    paths.e2e + '/flows/newflow.spec.js',
-    paths.e2e + '/flows/flows.spec.js',
-    paths.e2e + '/flows/newqueue.spec.js',
-    paths.e2e + '/flows/queues.spec.js'
+    //paths.e2e + '/login/login.spec.js',
+    //paths.e2e + '/navigation/sidebar.spec.js',
+    //paths.e2e + '/tableControls/**/*.spec.js',
+    //paths.e2e + '/management/**/*.spec.js',
+    //paths.e2e + '/configuration/**/*.spec.js',
+    //paths.e2e + '/userProfile/**/*.spec.js',
+    //paths.e2e + '/flows/newflow.spec.js',
+    //paths.e2e + '/flows/flows.spec.js',
+    //paths.e2e + '/flows/newqueue.spec.js',
+    //paths.e2e + '/flows/queues.spec.js'
+    paths.e2e + '/navigation/unsavedChanges.spec.js'
   ],
 
   framework: 'jasmine2',
