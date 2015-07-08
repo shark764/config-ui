2674627ced69e81c282cb387cc4a1cc116f24fe7
Greg Royan <greg.royan@bluespurs.com>
2015-07-08 12:11:59 -0300

    Bulk Action Skills commit

--- src/app/components/management/users/bulkActions/setSkillsBulkActionDirective.js
+++ src/app/components/management/users/bulkActions/setSkillsBulkActionDirective.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .directive('baSetSkills', [
-    function () {
+  .directive('baSetSkills', [ 'Skill', 'Session',
+    function (Skill, Session) {
       return {
         restrict: 'AE',
         scope: {
@@ -10,14 +10,44 @@ angular.module('liveopsConfigPanel')
         },
         templateUrl: 'app/components/management/users/bulkActions/setSkillsBulkAction.html',
         link: function ($scope) {
+          $scope.skillActions = [
+            {
+              action: 'add',
+              label:'Add Skill'
+            },
+            {
+              action: 'update',
+              label:'Update Skill'
+            },
+            {
+              action: 'remove',
+              label:'Remove Skill'
+            }
+          ];
+          var countSkills = 0;
+          $scope.skillsToAdd = [];
+
           $scope.skills = Skill.query({
             tenantId: Session.tenant.tenantId
           });
 
+          $scope.removeBulkSkill = function(item) {
+            var index = $scope.skillsToAdd.indexOf(item);
+            $scope.skillsToAdd.splice(index, 1);     
+          }
+
+          $scope.addBulkSkill = function() {
+            $scope.skillsToAdd.push({id: countSkills});
+            countSkills++;
+
+            console.log($scope.skillsToAdd);
+          }
+
           $scope.bulkAction.action = function(user) {
-            user.skills = $scope.skills
-            var promise = user.$update();
-            return promise;
+            console.log("Herro?");
+            //user.skills = $scope.skills;
+            console.log($scope.skillsToAdd);
+            return user;
           }
         }
       };
