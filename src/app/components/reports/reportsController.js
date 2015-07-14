4c5cecbdd50ec45af86f61b6237e32fc5d5dfcf3
Greg Royan <greg.royan@bluespurs.com>
2015-07-14 11:12:02 -0300

    Birst displaying in TITAN UI.

--- src/app/components/reports/reportsController.js
+++ src/app/components/reports/reportsController.js
@@ -3,11 +3,6 @@
 angular.module('liveopsConfigPanel')
   .controller('ReportsController', ['$scope', '$state',
     function($scope, $state) {
-      var optionsDashboard = {  
-        iframeSrc: "https://sde.birst.com/SSO.aspx?BirstSSOToken=" + $scope.token 
-        + "&birst.module=newDashboards&birst.exportZoom=2&&amp;birst.embedded=true&birst.hideDashboardNavigation=false&"
-        + "birst.hideDashboardPrompts=false&birst.dashboard=My%20Dashboard&birst.page=My%20Page" 
-      } ; 
-      BirstConfig.create("BirstDash", optionsDashboard); 
+     
     }
   ]);
