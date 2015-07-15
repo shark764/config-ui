248a08cdd27ed865cfa1b65a50508a98e19ed7a8
John Benson <john.benson@bluespurs.com>
2015-07-15 15:04:59 -0300

    TITAN2-1987 Added remove button to details and bulk actions. Bulk actions remove button will work once Phil's PR is pushed.

--- src/app/shared/directives/bulkActionExecutor/bulkActionExecutorDirective.js
+++ src/app/shared/directives/bulkActionExecutor/bulkActionExecutorDirective.js
@@ -14,6 +14,10 @@ angular.module('liveopsConfigPanel')
         link: function ($scope) {
           $scope.checkedItems = [];
 
+          $scope.closeBulk = function () {
+            $scope.bulkActions = null; // this will work when Phil pushes his PR.
+          };
+
           $scope.execute = function () {
             var selectedBulkActions = $scope.getSelectedItems($scope.bulkActions);
             var itemPromises = [];
