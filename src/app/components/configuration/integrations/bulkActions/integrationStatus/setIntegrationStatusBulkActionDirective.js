e751d155503563879826862e240268ae8b4199e1
samantha.routledge <samantha.routledge@bluespurs.com>
2015-07-17 15:49:22 -0300

    TITAN2-2203 Integrations bulk enable

---
+++ src/app/components/configuration/integrations/bulkActions/integrationStatus/setIntegrationStatusBulkActionDirective.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('liveopsConfigPanel')
+  .directive('baSetIntegrationStatus', ['Integration', 'Session',
+    function (Integration, Session) {
+      return {
+        restrict: 'AE',
+        scope: {
+          bulkAction: '='
+        },
+        templateUrl: 'app/components/configuration/integrations/bulkActions/integrationStatus/setIntegrationStatusBulkAction.html',
+        link: function ($scope) {
+          $scope.bulkAction.apply = function(integration) {
+            var integrationCopy = new Integration();
+            integrationCopy.id = integration.id;
+            integrationCopy.tenantId = Session.tenant.tenantId;
+            integrationCopy.status = $scope.status;
+            integrationCopy.properties = integration.properties;
+            return integrationCopy.save().then(function(integrationCopy) {
+              angular.copy(integrationCopy, integration);
+              integration.checked = true;
+              return integration;
+            });
+          };
+          
+          $scope.bulkAction.reset = function() {
+            $scope.bulkAction.checked = false;
+            $scope.status = false;
+          };
+        }
+      };
+    }
+  ]);
