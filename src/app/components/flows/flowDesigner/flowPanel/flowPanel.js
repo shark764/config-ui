bacefeed6cc996703566af1ffa76e517661edd55
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-07-21 11:17:11 -0300

    TITAN-2006, TITAN-2170, TITAN-1470

--- src/app/components/flows/flowDesigner/flowPanel/flowPanel.js
+++ src/app/components/flows/flowDesigner/flowPanel/flowPanel.js
@@ -3,8 +3,9 @@
 
   function addDefault (inputJSON, notation) {
     // Add default property if default is defined
-    if (_.isUndefined(inputJSON.default)) return;
-    return notation.params[inputJSON.name] = inputJSON.default;
+    if (_.isUndefined(inputJSON.default)) { return; }
+    notation.params[inputJSON.name] = inputJSON.default;
+    return;
   }
 
   function buildTemplate (notation) {
@@ -15,41 +16,41 @@
         var formSection = '<div class="input-group">'
         formSection += '<label>' + inputJSON.label + '</label>';
         formSection += '<div><input type="text" ng-model="notation.params.' + inputJSON.name + '"';
-        if (inputJSON.disabled === true) formSection += ' disabled="disabled"';
+        if (inputJSON.disabled === true) { formSection += ' disabled="disabled"'; }
         formSection += '></input></div></div>';
         return formSection;
       },
       textarea: function (inputJSON) {
-        var formSection = '<div class="input-group">'
+        var formSection = '<div class="input-group">';
         formSection += '<label>' + inputJSON.label + '</label>';
         formSection += '<div><textarea ng-model="notation.params.' + inputJSON.name + '"';
-        if (inputJSON.disabled === true) formSection += ' disabled="disabled"';
+        if (inputJSON.disabled === true) { formSection += ' disabled="disabled"'; }
         formSection += '></textarea></div></div>';
         return formSection;
       },
       select: function (inputJSON) {
-        var formSection = '<div class="input-group">'
+        var formSection = '<div class="input-group">';
         formSection += '<label>' + inputJSON.label + '</label>';
         formSection += '<div><select ng-model="notation.params.' + inputJSON.name + '"';
-        if (inputJSON.disabled === true) formSection += ' disabled="disabled"';
+        if (inputJSON.disabled === true) { formSection += ' disabled="disabled"'; }
         formSection += '><option value="undefined">Select one...</option></select></div></div>';
         return formSection;
       },
       keyValList: function (inputJSON) {
-        var formSection = '<div class="input-group">'
+        var formSection = '<div class="input-group">';
         formSection += '<label>' + inputJSON.label + '</label>';
         formSection += '<div><keyValList></keyValList></div></div>';
         return formSection;
       },
       boolean: function (inputJSON) {
-        var formSection = '<div class="input-group">'
+        var formSection = '<div class="input-group">';
         formSection += '<label>' + inputJSON.label + '</label>';
         formSection += '<toggle class="status-toggle"><label class="switch switch-green"><input type="checkbox" class="switch-input" ng-model="notation.params.' + inputJSON.name + '"';
-        if (inputJSON.disabled === true) formSection += ' disabled="disabled"';
+        if (inputJSON.disabled === true) { formSection += ' disabled="disabled"'; }
         formSection += '><span class="switch-label" data-on="On" data-off="Off"></span><span class="switch-handle"></span></label></toggle></div>';
         return formSection;
       }
-    }
+    };
 
     console.log('Notation:', notation);
 
@@ -61,14 +62,14 @@
     return tpl += '</div></div></form>';
   }
 
-  var flowPanel = function ($compile, $timeout) {
+  var flowPanel = function ($compile, $timeout, $window) {
     return {
       scope: {
         notation: '=notation'
       },
       template: '<div class="propsPanel"><h1 ng-show="loading"><i class="fa fa-spinner fa-spin"></i></h1></div>',
       restrict: 'E',
-      link: function (scope, element, attrs) {
+      link: function (scope, element) {
         scope.loading = true;
 
         console.log('NOTATION PASSED TO DIRECTIVE:', scope.notation);
@@ -80,7 +81,7 @@
           scope.loading = false;
         });
 
-        window.scope = scope;
+        $window.scope = scope;
       }
     };
   };
