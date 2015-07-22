0c8fc9e0e907ddc026bbb1f34f1e4bea7a2670f9
samantha.routledge <samantha.routledge@bluespurs.com>
2015-07-22 15:04:15 -0300

    TITAN2-2267 When model is null, it was trying to set the model to "+null" for phone numbers

--- src/app/shared/directives/resourceDetails/resourceDetailsDirective.js
+++ src/app/shared/directives/resourceDetails/resourceDetailsDirective.js
@@ -117,17 +117,22 @@ angular.module('liveopsConfigPanel')
         };
 
         $scope.resetForm = function () {
-          $scope.detailsForm.$setPristine();
-          $scope.detailsForm.$setUntouched();
-          
           //Workaround for fields with invalid text in them not being cleared when the model is updated to undefined
           //E.g. http://stackoverflow.com/questions/18874019/angularjs-set-the-model-to-be-again-doesnt-clear-out-input-type-url
           angular.forEach($scope.detailsForm, function (value, key) {
             if (value && value.hasOwnProperty('$modelValue') && value.$invalid){
-              $scope.detailsForm[key].$setViewValue(value.$modelValue);
+              var displayValue = value.$modelValue;
+              if (displayValue === null){
+                displayValue = undefined;
+              }
+              
+              $scope.detailsForm[key].$setViewValue(displayValue);
               $scope.detailsForm[key].$rollbackViewValue();
             }
           });
+          
+          $scope.detailsForm.$setPristine();
+          $scope.detailsForm.$setUntouched();
         };
 
         $scope.$on('resource:details:' + $scope.resourceName + ':cancel', function() {
