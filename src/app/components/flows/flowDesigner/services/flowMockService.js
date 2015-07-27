3b7216f770f06aa8b3cd7e5bb70f63683eef52fb
Nick Fitzpatrick <nickfitz.582@gmail.com>
2015-07-26 21:29:09 -0300

    changing terminate to a select

--- src/app/components/flows/flowDesigner/services/flowMockService.js
+++ src/app/components/flows/flowDesigner/services/flowMockService.js
@@ -122,7 +122,53 @@
             },
             'terminator': {
               'label': 'Terminator',
-              'type': 'text',
+              'type': 'select',
+              'options': [
+                {
+                  'value': '"#"',
+                  'content': '#'
+                },
+                {
+                  'value': '"0"',
+                  'content': '0'
+                },
+                {
+                  'value': '"1"',
+                  'content': '1'
+                },
+                {
+                  'value': '"2"',
+                  'content': '2'
+                },
+                {
+                  'value': '"3"',
+                  'content': '3'
+                },
+                {
+                  'value': '"4"',
+                  'content': '4'
+                },
+                {
+                  'value': '"5"',
+                  'content': '5'
+                },
+                {
+                  'value': '"6"',
+                  'content': '6'
+                },
+                {
+                  'value': '"7"',
+                  'content': '7'
+                },
+                {
+                  'value': '"8"',
+                  'content': '8'
+                },
+                {
+                  'value': '"9"',
+                  'content': '9'
+                }
+              ],
               'group': 'params'
             },
           },
