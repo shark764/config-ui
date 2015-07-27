8df83bdf9b64a55bf0d20370674a357a1140efdf
Sasha Wilcox <sasha.wilcox@bluespurs.com>
2015-07-27 16:12:42 -0300

    fixing wrong identifier

--- e2e/navigation/navigation.spec.js
+++ e2e/navigation/navigation.spec.js
@@ -13,53 +13,51 @@ describe('The view navigation', function() {
     shared.tearDown();
   });
 
-  it('should redirect to login page when not logged in and there is no page specified', function() {
-    browser.get(shared.root);
-    expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+  describe('when not logged in', function() {
+    it('should redirect to login page when there is no page specified', function() {
+      browser.get(shared.rootURL);
+      expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+    });
+
+    it('should redirect to login page when not logged in', function() {
+      browser.get(shared.usersPageUrl);
+      expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+
+      browser.get(shared.tenantsPageUrl);
+      expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+
+      browser.get(shared.profilePageUrl);
+      expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+    });
+
+    it('should navigate to login page for unknown urls and not logged in', function() {
+      browser.get(shared.mainUrl + 'unknownpage');
+      expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+    });
   });
 
-  it('should redirect to login page when not logged in', function() {
-    browser.get(shared.usersPageUrl);
-    expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+  describe('when logged in', function() {
+    it('should navigate to correct page', function() {
+      loginPage.login(params.login.user, params.login.password);
 
-    browser.get(shared.tenantsPageUrl);
-    expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+      browser.get(shared.usersPageUrl);
+      expect(browser.getCurrentUrl()).toContain(shared.usersPageUrl);
 
-    browser.get(shared.profilePageUrl);
-    expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
+      browser.get(shared.tenantsPageUrl);
+      expect(browser.getCurrentUrl()).toContain(shared.tenantsPageUrl);
 
-    browser.get(shared.invitesPageUrl);
-    expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
-  });
+      browser.get(shared.profilePageUrl);
+      expect(browser.getCurrentUrl()).toBe(shared.profilePageUrl);
+    });
 
-  it('should navigate to login page for unknown urls and not logged in', function() {
-    browser.get(shared.mainUrl + 'unknownpage');
-    expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
-  });
+    it('should redirect to User Management page when there is no page specified', function() {
+      browser.get(shared.rootURL);
+      expect(browser.getCurrentUrl()).toBe(shared.usersPageUrl);
+    });
 
-  it('should navigate to correct page when logged in', function() {
-    loginPage.login(params.login.user, params.login.password);
-
-    browser.get(shared.usersPageUrl);
-    expect(browser.getCurrentUrl()).toContain(shared.usersPageUrl);
-
-    browser.get(shared.tenantsPageUrl);
-    expect(browser.getCurrentUrl()).toContain(shared.tenantsPageUrl);
-
-    browser.get(shared.profilePageUrl);
-    expect(browser.getCurrentUrl()).toBe(shared.profilePageUrl);
-
-    browser.get(shared.invitesPageUrl);
-    expect(browser.getCurrentUrl()).toBe(shared.invitesPageUrl);
-  });
-
-  it('should redirect to User Management page when logged in when there is no page specified', function() {
-    browser.get(shared.root);
-    expect(browser.getCurrentUrl()).toBe(shared.loginPageUrl);
-  });
-
-  it('should navigate to main page for unknown urls and logged in', function() {
-    browser.get(shared.mainUrl + 'unknownpage');
-    expect(browser.getCurrentUrl()).toContain(shared.usersPageUrl);
+    it('should navigate to main page for unknown urls', function() {
+      browser.get(shared.mainUrl + 'unknownpage');
+      expect(browser.getCurrentUrl()).toContain(shared.usersPageUrl);
+    });
   });
 });
