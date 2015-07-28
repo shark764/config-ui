bec2712ac3d3f0758b1459fe67ddaf2e50b98e6b
Phil Hachey <phil.hachey@bluespurs.com>
2015-07-28 14:50:54 -0300

    TITAN2-2291 added more complexity to media collections duplcate validator
    Conflicts:
    	src/app/components/flows/media-collections/mediaCollectionDetailBody.html

--- src/app/components/flows/media-collections/duplicateValidator/duplicateValidator.js
+++ src/app/components/flows/media-collections/duplicateValidator/duplicateValidator.js
@@ -3,16 +3,31 @@
 angular.module('liveopsConfigPanel')
   .directive('mediaMapDuplicate', ['MediaCollection', function () {
     return {
-      require: 'ngModel',
-      link: function (scope, elm, attrs, ctrl) {
-        ctrl.$validators.mediaMapDuplicate = function(modelValue) {
-          var isValid = true;
-          var resource = scope.$parent.$parent.resource;
-          angular.forEach(resource.mediaMap, function(map) {
-            isValid = isValid && (map.lookup !== modelValue);
+      scope: {
+        form: '=',
+        resource: '='
+      },
+      link: function ($scope, $elm, $attrs, $ctrl) {
+        $scope.$watch('resource.mediaMap', function(newMediaMap) {
+          var mapValid = [];
+          angular.forEach(newMediaMap, function(outterMap, outterIndex) {
+            angular.forEach(newMediaMap, function(innerMap, innerIndex) {
+              if(outterMap === innerMap) {
+                return;
+              }
+              
+              var isValid = outterMap.lookup !== innerMap.lookup;
+              
+              mapValid[outterIndex] = mapValid[outterIndex] !== false && isValid;
+              mapValid[innerIndex] = mapValid[innerIndex] !== false && isValid;
+              
+              $scope.form['mapping' + outterIndex].$setValidity('mediaMapDuplicate', mapValid[outterIndex]);
+              $scope.form['mapping' + innerIndex].$setValidity('mediaMapDuplicate', mapValid[innerIndex]);
+            });
           });
-          return isValid;
-        };
+          
+          return true;
+        }, true)
       }
     };
   }]);
