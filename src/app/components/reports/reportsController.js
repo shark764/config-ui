82b87c9295e12b7987452a38943c2a066290e4a0
Greg Royan <greg.royan@bluespurs.com>
2015-07-31 12:58:58 -0300

    Removing groups from token generation

--- src/app/components/reports/reportsController.js
+++ src/app/components/reports/reportsController.js
@@ -9,9 +9,9 @@ angular.module('liveopsConfigPanel')
       $scope.fetch = function() {
 
         var username = 'titan-product@liveops.com';
-        var ssopassword = 'JO4IIiv0vuzyhoYoyWpbip0QquoCQyGh';
-        var spaceId = '2846b565-23f8-4032-b563-21f8b7a01cc5';
-        var sessionVars = 'tenant-id=\''+ Session.tenant.tenantId +'\';Birst$Groups=\'ExploreData\',\'ScheduleReports\',\'DownloadData\'';
+        var ssopassword = 'JO4IIiv0vuzyhoYoyWpbip0QquoCQyGh'; // Beta 4 Warehouse
+        var spaceId = '2846b565-23f8-4032-b563-21f8b7a01cc5'; // Beta 4 Warehouse
+        var sessionVars = 'tenant-id=\''+ Session.tenant.tenantId +'\';';
 
         $http({
           url: BIRST_URL + '/TokenGenerator.aspx?',
@@ -46,12 +46,17 @@ angular.module('liveopsConfigPanel')
       $scope.buildUrl = function() {
         var buildingUrl = BIRST_URL + '/SSO.aspx?';
 
-        var dashboardName = encodeURIComponent('Sample Dashboard 1');
+        var dashboardName = ''; // Name of the collection...
+        var pageName = '';  // Name of the Dashboard...  (Yes... variable naming fail by Birst.)
 
         buildingUrl = buildingUrl + 'birst.SSOToken=' + $scope.birst.SSOToken + '&birst.embedded=true&birst.module=' + $scope.birst.module;
 
         if ($scope.birst.module == 'newDashboards'){
           buildingUrl = buildingUrl + '&birst.hideDashboardNavigation=true';
+        }
+
+        if ( dashboardName != '' && pageName != '' ){
+          buildingUrl = buildingUrl + '&birst.dashbaord=' + dashboardName + '&birst.page=' + pageName;
         }
 
         $scope.birstUrl = $sce.trustAsResourceUrl(buildingUrl);
