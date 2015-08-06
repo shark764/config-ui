e60fc1c46e32bbddedabf9e1a742476399434208
Sam Routledge <samantha.routledge@bluespurs.com>
2015-08-06 18:34:54 -0300

    TITAN2-2676 Add E2E test for group members links

--- e2e/management/groups.spec.js
+++ e2e/management/groups.spec.js
@@ -4,6 +4,7 @@ describe('The groups view', function() {
   var loginPage = require('../login/login.po.js'),
     shared = require('../shared.po.js'),
     groups = require('./groups.po.js'),
+    users = require('./users.po.js'),
     params = browser.params,
     groupCount,
     randomGroup;
@@ -272,5 +273,33 @@ describe('The groups view', function() {
 	    expect(groups.nameFormField.getAttribute('disabled')).toBeTruthy();
 	  }
     });
+  });
+  
+  it('should link to the user details view in the members list', function() {
+	// Find a group with at least one member
+    shared.tableElements.then(function(rows) {
+    	var foundGroupWithMembers = false;
+    	
+        for (var i = 1; i <= rows.length; ++i) {
+          var row = element(by.css('tr.ng-scope:nth-child(' + i + ')'));
+          row.element(by.css(groups.membersColumn)).getText().then(function(value) {
+        	// Check if member count is greater than 0
+            if (!foundGroupWithMembers && parseInt(value) > 0) {
+              foundGroupWithMembers = true; //Set flag so we only do this once
+              row.click();
+              
+              //Save the member's name
+              var memberName = groups.groupMembersRows.get(0).getText();
+              
+              //Follow the link
+              groups.groupMembersRows.get(0).element(by.css('a')).click().then(function(){
+            	  expect(browser.getCurrentUrl()).toContain(shared.usersPageUrl);
+            	  expect(shared.detailsForm.isDisplayed()).toBeTruthy();
+            	  expect(users.userNameDetailsHeader.getText()).toContain(memberName);
+              });
+            }
+          });
+        }
+    })
   });
 });
