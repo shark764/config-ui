1caf30c19f2ab6e448da885758f3abddd48bfcb8
samantha.routledge <samantha.routledge@bluespurs.com>
2015-08-13 14:45:37 -0300

    TITAN2-2413 Remove extra invites folder and files

--- src/app/components/invites/invitesService.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('liveopsConfigPanel')
-  .factory('Invite', ['LiveopsResourceFactory', function (LiveopsResourceFactory) {
-
-    return LiveopsResourceFactory.create('/v1/tenants/:tenantId/invites/:verb', 'Invite');
-  }]);
-
