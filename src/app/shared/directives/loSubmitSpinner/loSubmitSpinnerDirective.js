e80e35d1dcc8bc054431f7611dc079d201c690d0
samantha.routledge <samantha.routledge@bluespurs.com>
2015-08-14 16:41:02 -0300

    TITAN2-2413 Fis lo-spinner button being shown all the time

--- src/app/shared/directives/loSubmitSpinner/loSubmitSpinnerDirective.js
+++ src/app/shared/directives/loSubmitSpinner/loSubmitSpinnerDirective.js
@@ -9,6 +9,7 @@ angular.module('liveopsConfigPanel')
       link: function($scope, ele) {
         $scope.spinnerElement = angular.element('<a disabled="true"><i class="fa fa-refresh fa-spin"></i></a>');
         $scope.spinnerElement.addClass(ele[0].className);
+        $scope.spinnerElement.addClass('ng-hide');
         ele.after($scope.spinnerElement);
 
         $scope.$watch('loSubmitSpinnerStatus', function (val) {
