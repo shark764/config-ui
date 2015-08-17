f59f16603884a4bf20cfaa49a78aa23d8d6ba2e5
samantha.routledge <samantha.routledge@bluespurs.com>
2015-08-17 13:41:31 -0300

    TITAN2-2413 Update ContentController unit tests

--- src/app/components/content/ContentController.spec.js
+++ src/app/components/content/ContentController.spec.js
@@ -1,8 +1,7 @@
 'use strict';
 
 describe('ContentController', function () {
-  var $scope,
-      Session;
+  var $scope;
 
   beforeEach(module('liveopsConfigPanel'));
   beforeEach(module('gulpAngular'));
@@ -12,8 +11,45 @@ describe('ContentController', function () {
     function ($rootScope, $controller) {
       $scope = $rootScope.$new();
       
-      Session = {};
-      $controller('ContentController', {'$scope': $scope, 'Session': Session});
+      $controller('ContentController', {'$scope': $scope});
     }
   ]));
+  
+  it('should catch the table create event and hide bulk actions panel', inject(['$rootScope', function($rootScope){
+    $scope.showBulkActions = true;
+    $rootScope.$broadcast('table:on:click:create');
+    $scope.$digest();
+    expect($scope.showBulkActions).toBeFalsy();
+  }]));
+  
+  it('should catch the item selected event and hide bulk actions panel', inject(['$rootScope', function($rootScope){
+    $scope.showBulkActions = true;
+    $rootScope.$broadcast('table:resource:selected');
+    $scope.$digest();
+    expect($scope.showBulkActions).toBeFalsy();
+  }]));
+  
+  it('should catch the action button click event and show the bulk action panel', inject(['$rootScope', function($rootScope){
+    $scope.showBulkActions = false;
+    $rootScope.$broadcast('table:on:click:actions');
+    $scope.$digest();
+    expect($scope.showBulkActions).toBeTruthy();
+  }]));
+  
+  it('should show an alert info if the messageKey url param is defined', inject(['$stateParams', 'Alert', '$controller', function($stateParams, Alert, $controller){
+    spyOn(Alert, 'info');
+    $stateParams.messageKey = 'message.key.value';
+    $controller('ContentController', {'$scope': $scope});
+    expect(Alert.info).toHaveBeenCalledWith('message.key.value', '', jasmine.any(Object));
+  }]));
+  
+  it('should reset the queryCache if Session tenant changes', inject(['queryCache', 'Session', function(queryCache, Session){
+    spyOn(queryCache, 'removeAll');
+    Session.tenant = {
+        tenantId: 'anotherId'
+    };
+    
+    $scope.$digest();
+    expect(queryCache.removeAll).toHaveBeenCalled();
+  }]));
 });
