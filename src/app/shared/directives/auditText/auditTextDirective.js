622f9558f958aab030160f327bfe271212b9faa8
Phil Hachey <phil.hachey@bluespurs.com>
2015-08-20 15:05:49 -0300

    TITAN2-2892 Removed uuidCache and userCache
    Replaced with User.cachedGet and fixed up cachedGet

--- src/app/shared/directives/auditText/auditTextDirective.js
+++ src/app/shared/directives/auditText/auditTextDirective.js
@@ -1,45 +1,40 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .directive('auditText', ['$filter', 'UserCache', function ($filter, UserCache) {
-    return {
-      restrict: 'AE',
-      scope: {
-        translation: '@',
-        userId: '=',
-        date: '='
-      },
-      link: function ($scope, element, attributes) {
-        $scope.getAttributes = function() {
-          var attrs = {};
-          angular.forEach(attributes.$attr, function(value, index) {
-            attrs[index] = attributes[index];
-          });
-
-          return attrs;
-        };
-
-        $scope.refresh = function () {
-          var filter = $filter('translate');
-          var attrs = $scope.getAttributes();
+  .directive('auditText', ['$filter', 'User',
+    function ($filter, User) {
+      return {
+        restrict: 'AE',
+        scope: {
+          translation: '@',
+          userId: '=',
+          date: '='
+        },
+        template: '{{get()}}',
+        link: function ($scope, element, attributes) {
+          $scope.text;
           
-          if($scope.userId) {
-            attrs.displayName = filter('value.unknown');
-            attrs.date = $filter('date')($scope.date, 'medium');
-            
-            var promise = UserCache.get($scope.userId).$promise;
-            promise.then(function (user) {
-              attrs.displayName = user.getDisplay();
-              element.text(filter($scope.translation, attrs));
-            });
-          } else {
-            element.text(filter($scope.translation, attrs));
-          }
-        };
+          $scope.get = function () {
+            if (!$scope.userId) {
+              return  $filter('translate')($scope.translation, {
+                date: $filter('date')($scope.date, 'medium')
+              });
+            }
 
-        $scope.$watchGroup(['userId', 'date'], function() {
-          $scope.refresh();
-        });
-      }
-    };
-  }]);
+            var user = User.cachedGet({
+              id: $scope.userId
+            });
+
+            if(user.$resolved) {
+              return $scope.text = $filter('translate')($scope.translation, {
+                displayName: user.getDisplay(),
+                date: $filter('date')($scope.date, 'medium')
+              });
+            }
+
+            return $scope.text;
+          };
+        }
+      };
+    }
+  ]);
