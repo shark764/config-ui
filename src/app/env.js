72dd46fbb76a28e3286cc3033894badd78386df7
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-08-21 16:11:35 -0300

    Tweaks

--- src/app/env.js
+++ src/app/env.js
@@ -1,5 +1,4 @@
 angular.module('liveopsConfigPanel.config')
-  // .constant('apiHostname', 'http://localhost:9080')
   .constant('apiHostname', 'https://dev-api.liveopslabs.com')
   .constant('BIRST_URL', 'http://dev-birst.liveopslabs.com')
   .constant('SSO_PASSWORD', 'JO4IIiv0vuzyhoYoyWpbip0QquoCQyGh')
