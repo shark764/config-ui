73b88141166d94f3f55a933384546c4f0a953d81
samantha.routledge <samantha.routledge@bluespurs.com>
2015-08-27 14:56:06 -0300

    TITAN2-3248 Refresh session tenants when user is tenant admin

--- src/app/components/configuration/tenants/tenantsController.js
+++ src/app/components/configuration/tenants/tenantsController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .controller('TenantsController', ['$scope', '$stateParams', '$filter', 'Session', 'Tenant', 'TenantUser', 'tenantTableConfig', 'BulkAction', 'UserPermissions', '$q',
-    function($scope, $stateParams, $filter, Session, Tenant, TenantUser, tenantTableConfig, BulkAction, UserPermissions, $q) {
+  .controller('TenantsController', ['$scope', '$stateParams', '$filter', 'Session', 'Tenant', 'TenantUser', 'tenantTableConfig', 'BulkAction', 'UserPermissions', 'AuthService',
+    function($scope, $stateParams, $filter, Session, Tenant, TenantUser, tenantTableConfig, BulkAction, UserPermissions, AuthService) {
 
       $scope.create = function() {
         $scope.selectedTenant = new Tenant({
@@ -29,6 +29,19 @@ angular.module('liveopsConfigPanel')
 
       $scope.$on('table:on:click:create', function() {
         $scope.create();
+      });
+      
+      $scope.$on('resource:details:tenants:create:success', function(event, newTenant) {
+        if (newTenant.adminUserId === Session.user.id){
+          AuthService.refreshTenants();
+        }
+      });
+      
+      $scope.$on('resource:details:tenants:update:success', function(event, updatedTenant) {
+        //TODO: What happens if user was an admin on the tenant, then changed it to someone else?
+        if (updatedTenant.adminUserId === Session.user.id){
+          AuthService.refreshTenants();
+        }
       });
 
       $scope.tableConfig = tenantTableConfig;
