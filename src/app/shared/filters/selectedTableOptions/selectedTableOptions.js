d0571fdd380feb49988bb2024dee091a3ab80f50
samantha.routledge <samantha.routledge@bluespurs.com>
2015-09-03 16:20:43 -0300

    TITAN2-3434 Allow multiple table filters to be combined
    - A table row will only be displayed if it matches all the column
    filters, not if it matches *any* of them.

--- src/app/shared/filters/selectedTableOptions/selectedTableOptions.js
+++ src/app/shared/filters/selectedTableOptions/selectedTableOptions.js
@@ -11,31 +11,70 @@ angular.module('liveopsConfigPanel')
         }
         
         var nothingChecked = true;
-        for(var fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
-          var field = fields[fieldIndex];
-          if(!$parse('header.options')(field)) {
-            continue;
-          }
-          
-          var options = $filter('invoke')(field.header.options);
-          
-          for(var optionIndex = 0; optionIndex < options.length; optionIndex++) {
-            var option = options[optionIndex];
-            nothingChecked = nothingChecked && !option.checked;
-            if(!option.checked){
+//        for(var fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
+//          var field = fields[fieldIndex];
+//          if(!$parse('header.options')(field)) {
+//            continue;
+//          }
+//          
+//          var options = $filter('invoke')(field.header.options);
+ //         
+//          for(var optionIndex = 0; optionIndex < options.length; optionIndex++) {
+//            var option = options[optionIndex];
+//            nothingChecked = nothingChecked && !option.checked;
+//            if(!option.checked){
+//              continue;
+//            }
+//            
+//            var parseValue = $parse(field.header.valuePath ? field.header.valuePath : 'value');
+//            var value = $filter('invoke')(parseValue(option), option);
+//            
+//            for(var itemIndex = 0; itemIndex < items.length; itemIndex++) {
+//              var item = items[itemIndex];
+//              var lookup = field.lookup ? field.lookup : field.name;
+//              if ($filter('matchesField')(item, lookup, value)) {
+//                filtered.push(item);
+//              }
+//            }
+//          }
+//        }
+        
+        for(var itemIndex = 0; itemIndex < items.length; itemIndex++) {
+          var item = items[itemIndex];
+          var showItemInTable = true;
+          for(var fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
+            var matchesColumnFilter = true;
+            var field = fields[fieldIndex];
+            if(!$parse('header.options')(field)) {
               continue;
             }
             
-            var parseValue = $parse(field.header.valuePath ? field.header.valuePath : 'value');
-            var value = $filter('invoke')(parseValue(option), option);
+            var lookup = field.lookup ? field.lookup : field.name;
+            var options = $filter('invoke')(field.header.options);
             
-            for(var itemIndex = 0; itemIndex < items.length; itemIndex++) {
-              var item = items[itemIndex];
-              var lookup = field.lookup ? field.lookup : field.name;
+            for(var optionIndex = 0; optionIndex < options.length; optionIndex++) {
+              var option = options[optionIndex];
+              nothingChecked = nothingChecked && !option.checked;
+              if(!option.checked){
+                continue;
+              }
+              
+              var parseValue = $parse(field.header.valuePath ? field.header.valuePath : 'value');
+              var value = $filter('invoke')(parseValue(option), option);
+              
               if ($filter('matchesField')(item, lookup, value)) {
-                filtered.push(item);
+                matchesColumnFilter = true;
+                break;
+              } else {
+                matchesColumnFilter = false;
               }
             }
+            
+            showItemInTable = showItemInTable && matchesColumnFilter;
+          }
+          
+          if (showItemInTable){
+            filtered.push(item);
           }
         }
 
