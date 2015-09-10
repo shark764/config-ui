19906dd67c6c533ed161f906ee547982d57ba7fe
Sasha Wilcox <sasha.wilcox@bluespurs.com>
2015-09-10 12:19:23 -0300

    TITAN2-3161 Tenant added to top nav when creating, added TODOs for unknown expected results

--- e2e/configuration/newTenant.spec.js
+++ e2e/configuration/newTenant.spec.js
@@ -30,13 +30,13 @@ describe('The create new tenants view', function() {
     expect(tenants.adminFormDropDown.isDisplayed()).toBeTruthy();
 
     // Defaults admin to current user
-    expect(tenants.adminFormDropDown.$('option:checked').getText()).toBe(params.login.firstName + ' '+ params.login.lastName);
+    expect(tenants.adminFormDropDown.$('option:checked').getText()).toBe(params.login.firstName + ' ' + params.login.lastName);
 
     // Region is not displayed when adding a new tenant, defaults to current region
     expect(tenants.region.isPresent()).toBeFalsy();
   });
 
-  it('should successfully create a new tenant and add to the tenants lists', function() {
+  it('should successfully create a new tenant and add to the tenants table and dropdown', function() {
     shared.createBtn.click();
     randomTenant = Math.floor((Math.random() * 1000) + 1);
     var tenantAdded = false;
@@ -44,7 +44,7 @@ describe('The create new tenants view', function() {
     // Complete tenant form and submit
     tenants.nameFormField.sendKeys('Tenant ' + randomTenant);
     tenants.descriptionFormField.sendKeys('This is the tenant description for tenant ' + randomTenant);
-    shared.submitFormBtn.click().then(function () {
+    shared.submitFormBtn.click().then(function() {
       expect(shared.successMessage.isDisplayed()).toBeTruthy();
 
       // Confirm tenant is displayed in tenant table with correct details
@@ -56,10 +56,93 @@ describe('The create new tenants view', function() {
             }
           });
         }
-      }).thenFinally(function() {
+      }).then(function() {
         // Verify new tenant was found in the table
         expect(tenantAdded).toBeTruthy();
         expect(shared.tableElements.count()).toBeGreaterThan(tenantCount);
+      }).then(function() {
+        // Reset flag
+        tenantAdded = false;
+
+        // Confirm tenant added to tenant dropdown
+        shared.tenantsNavDropdown.click();
+        expect(shared.tenantsNavDropdownContents.count()).toBe(shared.tableElements.count());
+
+        shared.tenantsNavDropdownContents.then(function(tenants) {
+          for (var i = 0; i < tenants.length; ++i) {
+            tenants[i].getText().then(function(value) {
+              if (value == 'Tenant ' + randomTenant) {
+                tenantAdded = true;
+              }
+            });
+          }
+        }).then(function() {
+          // Verify new tenant was found in the tenant dropdown
+          expect(tenantAdded).toBeTruthy();
+        });
+      });
+    });
+  });
+
+  xit('should not add new tenant to the tenants nav dropdown when current user is not selected as admin', function() {
+    // TODO Determine expected result
+    shared.createBtn.click();
+    randomTenant = Math.floor((Math.random() * 1000) + 1);
+    var tenantAdded = false;
+
+    // Complete tenant form and submit
+    tenants.nameFormField.sendKeys('Tenant ' + randomTenant);
+    tenants.descriptionFormField.sendKeys('This is the tenant description for tenant ' + randomTenant);
+
+    // Select a different admin
+    tenants.adminFormDropDown.all(by.css('option')).then(function(users) {
+      for (var i = 0; i < users.length; i++) {
+        users[i].getText().then(function(userName) {
+          // If not current user, select as admin
+          if (userName !== (params.login.firstName + params.login.lastName)) {
+            users[i].click();
+          }
+        });
+      }
+    }).then(function () {
+      shared.submitFormBtn.click().then(function() {
+        expect(shared.successMessage.isDisplayed()).toBeTruthy();
+
+        // Confirm tenant is displayed in tenant table with correct details
+        shared.tableElements.then(function(rows) {
+          for (var i = 1; i <= rows.length; ++i) {
+            element(by.css('tr.ng-scope:nth-child(' + i + ') > td:nth-child(2) > span:nth-child(1)')).getText().then(function(value) {
+              if (value == 'Tenant ' + randomTenant) {
+                tenantAdded = true;
+              }
+            });
+          }
+        }).then(function() {
+          // Verify new tenant was found in the table
+          expect(tenantAdded).toBeTruthy();
+          expect(shared.tableElements.count()).toBeGreaterThan(tenantCount);
+        }).then(function() {
+          tenantAdded = false;
+
+          // Confirm tenant is NOT added to tenant dropdown
+          shared.tenantsNavDropdown.click();
+
+          // Tenant nav count remains the previous tenant count
+          expect(shared.tenantsNavDropdownContents.count()).toBe(tenantCount);
+
+          shared.tenantsNavDropdownContents.then(function(tenants) {
+            for (var i = 0; i < tenants.length; ++i) {
+              tenants[i].getText().then(function(value) {
+                if (value == 'Tenant ' + randomTenant) {
+                  tenantAdded = true;
+                }
+              });
+            }
+          }).then(function() {
+            // Verify new tenant was NOT found in the tenant dropdown
+            expect(tenantAdded).toBeFalsy();
+          });
+        });
       });
     });
   });
