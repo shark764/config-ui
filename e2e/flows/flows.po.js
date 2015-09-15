cd4a90e63514f16c9abf8f3c7f6b3f69290c7c84
samantha.routledge <samantha.routledge@bluespurs.com>
2015-09-15 16:46:46 -0300

    Update versions selector for flows test

--- e2e/flows/flows.po.js
+++ e2e/flows/flows.po.js
@@ -4,11 +4,11 @@ var FlowPage = function() {
   this.nameFormField = element(by.model('selectedFlow.name'));
   this.descriptionFormField = element(by.model('selectedFlow.description'));
   this.typeFormDropdown = element(by.model('selectedFlow.type'));
-  this.activeFormToggle = element(by.model('resource.active'));
-  this.activeVersionDropdown = element(by.model('resource.activeVersion'));
+  this.activeFormToggle = element(by.model('selectedFlow.active'));
+  this.activeVersionDropdown = element(by.model('selectedFlow.activeVersion'));
 
   this.versionsTable = element(by.id('flow-details-versions-table'));
-  this.versionsTableElements =  this.versionsTable.all(by.repeater('version in versions | orderBy:\'created\':\'reverse\''));
+  this.versionsTableElements =  this.versionsTable.all(by.repeater('version in getVersions() | orderBy:\'created\':\'reverse\''));
   this.versionNameFormField = element(by.model('version.name'));
   this.versionDescriptionFormField = element(by.model('version.description'));
 
