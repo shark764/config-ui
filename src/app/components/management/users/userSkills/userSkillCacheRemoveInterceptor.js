e4d71d078878f102db606bc5e003230880bee491
samantha.routledge <samantha.routledge@bluespurs.com>
2015-09-30 11:17:42 -0300

    TITAN2-3859 Remove default cache key from userskillcacheremoveinterceptor

--- src/app/components/management/users/userSkills/userSkillCacheRemoveInterceptor.js
+++ src/app/components/management/users/userSkills/userSkillCacheRemoveInterceptor.js
@@ -4,16 +4,17 @@ angular.module('liveopsConfigPanel')
   .service('userSkillCacheRemoveInterceptor', ['queryCache', 'filterFilter',
     function (queryCache, filterFilter) {
       this.response = function (response) {
-        var proto = Object.getPrototypeOf(response.resource);
-        var keyName = response.resource.cacheKey ? response.resource.cacheKey() : proto.resourceName;
-
-        if(queryCache.get(keyName)) {
-          var cacheMatch = filterFilter(queryCache.get(keyName), {
-            skillId: response.resource.skillId
-          }, true);
+        if (angular.isDefined(response.resource.cacheKey())){
+          var keyName = response.resource.cacheKey();
           
-          if (cacheMatch.length > 0){
-            queryCache.get(keyName).removeItem(cacheMatch[0]);
+          if(queryCache.get(keyName)) {
+            var cacheMatch = filterFilter(queryCache.get(keyName), {
+              skillId: response.resource.skillId
+            }, true);
+            
+            if (cacheMatch.length > 0){
+              queryCache.get(keyName).removeItem(cacheMatch[0]);
+            }
           }
         }
 
