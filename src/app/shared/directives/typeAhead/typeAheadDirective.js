7b9e9ae34b10cd9e7f02fb105ab35f48f337b8f8
Nick Fitzpatrick <nickfitz.582@gmail.com>
2015-10-14 14:49:04 -0300

    small adjustments for directive changes

--- src/app/shared/directives/typeAhead/typeAheadDirective.js
+++ src/app/shared/directives/typeAhead/typeAheadDirective.js
@@ -13,14 +13,14 @@ angular.module('liveopsConfigPanel')
         keepExpanded: '=',
         onEnter: '&',
         filters: '=?',
-        selectedItem: '='
+        selectedItem: '=?'
       },
 
       templateUrl: 'app/shared/directives/typeAhead/typeAhead.html',
 
       controller: function($scope) {
         var self = this;
-        
+
         $scope.currentText = $scope.prefill || '';
 
         this.defaultTextFilter = function defaultTextFilter(item, text) {
@@ -42,19 +42,19 @@ angular.module('liveopsConfigPanel')
 
         $scope.$watch('filters', function(newCriteria) {
           $scope.filterArray = [];
-          
+
           if (newCriteria && angular.isArray(newCriteria)) {
             $scope.filterArray = angular.copy(newCriteria);
           } else if(newCriteria && !angular.isArray(newCriteria)) {
             $scope.filterArray = [newCriteria];
           }
-          
+
           $scope.filterArray.push(self.defaultTextFilter);
         }, true);
-        
+
         $scope.updateHighlight = function(){
           var filteredItems = $filter('filter')($scope.items, $scope.filterCriteria, true);
-          
+
           if ($scope.currentText === ''){
             $scope.highlightedItem = null;
             $scope.selectedItem = null;
@@ -65,7 +65,7 @@ angular.module('liveopsConfigPanel')
               $scope.highlightedItem = null;
               $scope.selectedItem = $scope.currentText;
             }
-            
+
             if (angular.isDefined(filteredItems[0].getDisplay) && filteredItems[0].getDisplay() === $scope.currentText){
               //If the input exactly matches a result
               $scope.highlightedItem = filteredItems[0];
@@ -79,17 +79,17 @@ angular.module('liveopsConfigPanel')
             $scope.selectedItem = $scope.currentText;
           }
         };
-        
+
         $scope.$watch('currentText', function() {
           $scope.updateHighlight();
         });
-        
+
         $scope.$watch('selectedItem', function(newVal) {
           if (newVal === null){
             $scope.currentText = '';
           }
         });
-        
+
         $scope.$watch('items', function(items) {
           if (angular.isDefined(items)){
             $scope.updateHighlight();
@@ -100,10 +100,10 @@ angular.module('liveopsConfigPanel')
           if (! angular.isString(item)){
             $scope.currentText = angular.isDefined(item.getDisplay) ? item.getDisplay() : item[$scope.nameField];
           }
-          
+
           $scope.selectedItem = item;
           $scope.onSelect({selectedItem: item});
-          
+
           if (!$scope.keepExpanded) {
             $scope.hovering = false;
             $scope.showSuggestions = false;
@@ -115,24 +115,24 @@ angular.module('liveopsConfigPanel')
             $scope.showSuggestions = false;
           }
         };
-        
+
         $scope.orderByFunction = function(item){
           var displayString = item.getDisplay();
-          
+
           return displayString? displayString : item[$scope.nameField];
         };
       },
       link: function($scope, element) {
         element.find('input').bind('keydown keypress', function(event){
           var highlightedIndex;
-          
+
           if (event.which === 13) { //Enter key
             $timeout(function(){
               var selected = $scope.highlightedItem ? $scope.highlightedItem : $scope.currentText;
               $scope.select(selected);
               $scope.onEnter({item: selected});
             });
-            
+
             event.preventDefault();
           } else if(event.which === 40){ //Down arrow key
            highlightedIndex = $scope.filtered.indexOf($scope.highlightedItem);
@@ -140,7 +140,7 @@ angular.module('liveopsConfigPanel')
             if (highlightedIndex + 1 < $scope.filtered.length){
               $timeout(function(){
                 $scope.highlightedItem = $scope.filtered[highlightedIndex + 1];
-                
+
                 var li = element.find('li:nth-child(' + (highlightedIndex + 2) + ')');
                 $scope.showListElement(li);
               });
@@ -151,7 +151,7 @@ angular.module('liveopsConfigPanel')
             if (highlightedIndex - 1 >= 0){
               $timeout(function(){
                 $scope.highlightedItem = $scope.filtered[highlightedIndex - 1];
-                
+
                 //Scroll to this element in the dropdown
                 var li = element.find('li:nth-child(' + highlightedIndex + ')');
                 $scope.showListElement(li);
@@ -159,14 +159,14 @@ angular.module('liveopsConfigPanel')
             }
           }
         });
-        
+
         $scope.showListElement = function(li){
           var elementTop = li.get(0).offsetTop;
           var elementHeight = li.get(0).offsetHeight;
           var elementBottom = elementTop + elementHeight;
           var containerHeight = element.find('ul').get(0).offsetHeight;
           var scrollTop = element.find('ul').get(0).scrollTop;
-          
+
           if (elementBottom > (scrollTop + containerHeight)){
             element.find('ul').get(0).scrollTop = elementBottom - containerHeight;
           } else if (elementTop < scrollTop){
