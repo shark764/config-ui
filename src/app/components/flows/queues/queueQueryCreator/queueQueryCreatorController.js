613f6b56c2d711b981dee9bfc9037a16220ef30b
samantha routledge <samantha.routledge@bluespurs.com>
2015-10-21 14:41:38 -0300

    TITAN2-4513 Add link to remove a filter section in query builder

--- src/app/components/flows/queues/queueQueryCreator/queueQueryCreatorController.js
+++ src/app/components/flows/queues/queueQueryCreator/queueQueryCreatorController.js
@@ -1,10 +1,8 @@
 'use strict';
 
-/*global jsedn: false */
-
 angular.module('liveopsConfigPanel')
-  .controller('QueueQueryCreatorController', ['$scope',
-    function($scope) {
+  .controller('QueueQueryCreatorController', ['$scope', 'jsedn', 'Alert', '$translate',
+    function($scope, jsedn, Alert, $translate) {
       $scope.$watch('rootMap', function(newMap) {
         if(!newMap || !$scope.version) {
           return;
@@ -21,21 +19,50 @@ angular.module('liveopsConfigPanel')
         $scope.rootMap = jsedn.parse(newQuery);
       });
       
-      $scope.queryComponents = {
-        'skillcomponent': {
-          display:'Skills',
-          key: 'skillcomponent'
-        }, 
-        'groupcomponent': {
-          display:'Groups',
-          key: 'groupcomponent'
+      $scope.queryComponents = [{
+        display: $translate.instant('queues.query.builder.skills'),
+        enabled: false,
+        name: 'skillcomponent',
+        keyword: ':skills',
+        remove: function(rootMap){
+          var keyword = jsedn.kw(':skills');
+          rootMap.remove(keyword);
         }
-      };
+      }, {
+        display: $translate.instant('queues.query.builder.groups'),
+        enabled: false,
+        name: 'groupcomponent',
+        keyword: ':groups',
+        remove: function(rootMap){
+          var keyword = jsedn.kw(':groups');
+          rootMap.remove(keyword);
+        }
+      }];
       
       $scope.add = function(selectedComponent){
         $scope.selectedComponent = null;
-        $scope[selectedComponent.key] = true;
-        delete $scope.queryComponents[selectedComponent.key];
+        $scope[selectedComponent.name] = selectedComponent;
+        selectedComponent.enabled = true;
+      };
+      
+      $scope.remove = function(selectedComponent){
+        var keyword = jsedn.kw(selectedComponent.keyword);
+        
+        function doRemove(){
+          $scope[selectedComponent.name] = null;
+          selectedComponent.enabled = false;
+          if ($scope.rootMap.exists(keyword)){
+            selectedComponent.remove($scope.rootMap);
+          }
+        }
+        
+        if ($scope.rootMap.exists(keyword) && $scope.rootMap.at(keyword).val.length > 0){
+          Alert.confirm($translate.instant('queue.query.builder.remove.filter.confirm'), function(){
+            doRemove();
+          });
+        } else {
+          doRemove();
+        }
       };
     }
   ]);
