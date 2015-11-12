78e234b830cddccbe0129856a2ba7a8aaaf34978
Phil Hachey <phil.hachey@bluespurs.com>
2015-11-12 15:04:21 -0400

    TITAN2-4698 fixed validation issues

--- src/app/components/configuration/lists/item/listItemDirective.js
+++ src/app/components/configuration/lists/item/listItemDirective.js
@@ -6,11 +6,20 @@ angular.module('liveopsConfigPanel')
       restrict: 'E',
       scope: {
         item: '=',
-        listType: '='
+        listType: '=',
+        index: '@'
       },
       templateUrl: 'app/components/configuration/lists/item/listItem.html',
       link: function($scope) {
+        $scope.inputChange = function inputChange(field) {
+          if ($scope.item[field.name] === null) {
+            delete $scope.item[field.name]
+          }
+        };
         
+        $scope.getName = function getName(field) {
+          return $scope.index + '.' + field.name;
+        }
       }
     }
   }]);
