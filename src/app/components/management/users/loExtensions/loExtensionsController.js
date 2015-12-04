4885efef4110845f8d6c85692dbec7203c1157db
samantha.routledge <samantha.routledge@bluespurs.com>
2015-12-04 16:32:26 -0400

    Refactor lo-extensions and add unit tests

---
+++ src/app/components/management/users/loExtensions/loExtensionsController.js
@@ -0,0 +1,76 @@
+'use strict';
+
+angular.module('liveopsConfigPanel')
+  .controller('LoExtensionsController', ['$scope', '$q', '$parse', 'Session', 'loExtensionProviders', 'loExtensionTypes',
+    function ($scope, $q, $parse, Session, loExtensionProviders, loExtensionTypes) {
+      var vm = this;
+      $scope.loExtensionProviders = loExtensionProviders;
+      $scope.loExtensionTypes = loExtensionTypes;
+
+      $scope.newExtension = {};
+
+      vm.save = function() {
+        delete $scope.tenantUser.status;
+        delete $scope.tenantUser.roleId;
+
+        return $scope.tenantUser.save({
+          tenantId : Session.tenant.tenantId
+        }).then(function (tenantUser) {
+          $scope.newExtension = {};
+          $scope.newExtension.type = 'webrtc';
+
+          angular.forEach([
+              'type', 'provider', 'value'
+          ], function (field) {
+            $scope.userTenantExtensionForm[field].$setPristine();
+            $scope.userTenantExtensionForm[field].$setUntouched();
+          });
+
+          return tenantUser;
+        }, function (error) {
+          var def = $q.defer();
+
+          $scope.tenantUser.reset();
+
+          def.reject(error);
+          return def.promise;
+        });
+      };
+
+      $scope.add = function () {
+        $scope.newExtension.value = $scope.phoneNumber;
+        if ($scope.phoneExtension) {
+          $scope.newExtension.value += 'x' + $scope.phoneExtension;
+        } else if ($scope.sipExtension) {
+          $scope.newExtension.value = $scope.sipExtension;
+        }
+
+        $scope.tenantUser.extensions.push($scope.newExtension);
+        return vm.save().then(function (tenantUser) {
+          $scope.clearValues();
+          return tenantUser;
+        });
+      };
+
+      $scope.clearValues = function () {
+
+        $scope.phoneNumber = null;
+        $scope.phoneExtension = null;
+        $scope.sipExtension = null;
+        $scope.newExtension.provider = null;
+
+      };
+
+      $scope.remove = function (extension) {
+        $scope.tenantUser.extensions.removeItem(extension);
+        return vm.save();
+      };
+
+      $scope.moved = function (index) {
+        $scope.tenantUser.extensions.splice(index, 1);
+        return vm.save();
+      };
+
+      $scope.newExtension.type = 'webrtc';
+    }
+  ]);
