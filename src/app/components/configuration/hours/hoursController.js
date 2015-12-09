2b5ca5b436bd8d270d52dc22c3e5a94c22cf3e4b
Phil Hachey <phil.hachey@bluespurs.com>
2015-12-09 16:52:25 -0400

    TITAN2-4975 added logic for saving and validation for business hours exceptions

--- src/app/components/configuration/hours/hoursController.js
+++ src/app/components/configuration/hours/hoursController.js
@@ -4,7 +4,9 @@ angular.module('liveopsConfigPanel')
   .controller('HoursController', ['$scope', '$filter', 'Session', 'BusinessHour', 'BusinessHourException', 'Timezone', 'hoursTableConfig', 'Alert',
     function($scope, $filter, Session, BusinessHour, BusinessHourException, Timezone, hoursTableConfig, Alert) {
       var vm = this;
-
+      
+      vm.dayPrefixes = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+      
       vm.loadTimezones = function() {
         $scope.timezones = Timezone.query();
       };
@@ -13,7 +15,7 @@ angular.module('liveopsConfigPanel')
         $scope.hours = BusinessHour.cachedQuery({
           tenantId: Session.tenant.tenantId
         });
-
+        
         return $scope.hours;
       };
 
@@ -22,12 +24,25 @@ angular.module('liveopsConfigPanel')
       };
 
       $scope.hasHours = function() {
-        for(var index in $scope.selectedHour) {
-          if(index.indexOf('StartTimeMinutes') > -1 ||
-            index.indexOf('EndTimeMinutes') > -1) {
+        if(!$scope.selectedHour) {
+          return false;
+        }
+        
+        var hasHours = false;
+        for(var index = 0; index < vm.dayPrefixes.length; index++) {
+          var dayPrefix = vm.dayPrefixes[index];
+          
+          hasHours = $scope.selectedHour[dayPrefix + 'StartTimeMinutes'] &&
+            $scope.selectedHour[dayPrefix + 'StartTimeMinutes'] !== -1;
+          
+          hasHours = hasHours || ($scope.selectedHour[dayPrefix + 'EndTimeMinutes'] &&
+            $scope.selectedHour[dayPrefix + 'EndTimeMinutes'] !== -1);
+          
+          if(hasHours) {
             return true;
           }
         }
+        return false;
       };
 
       $scope.showCreateException = function(){
@@ -56,7 +71,7 @@ angular.module('liveopsConfigPanel')
           Alert.success($filter('translate')('hours.exception.create.success'));
           return exceptionHour;
         }, function(error) {
-          Alert.success($filter('translate')('hours.exception.create.failure'));
+          Alert.error($filter('translate')('hours.exception.create.failure'));
           return error;
         });
       };
@@ -66,12 +81,21 @@ angular.module('liveopsConfigPanel')
           businessHourId: $scope.selectedHour.id
         }).then(function() {
           $scope.selectedHour.$exceptions.removeItem(exception);
-          Alert.success($filter('translate')('hours.exception.removed.success'));
+          Alert.success($filter('translate')('hours.exception.remove.success'));
         }, function() {
-          Alert.error($filter('translate')('hours.exception.removed.failure'));
+          Alert.error($filter('translate')('hours.exception.remove.failure'));
         });
       };
-
+      
+      $scope.onIsHoursCustomChanged = function(isCustom) {
+        if(!isCustom) {
+          angular.forEach(vm.dayPrefixes, function(dayPrefix) {
+            $scope.selectedHour[dayPrefix + 'StartTimeMinutes'] = -1;
+            $scope.selectedHour[dayPrefix + 'EndTimeMinutes'] = -1;
+          });
+        }
+      };
+      
       $scope.generateHoursMessage = function(day) {
         return {
           day: $filter('translate')('hours.' + day)
@@ -87,16 +111,26 @@ angular.module('liveopsConfigPanel')
 
         return hours + ':' + minutesRemainder;
       };
-
+      
+      $scope.$watch('selectedHour', function() {
+        $scope.isHoursCustom = $scope.hasHours();
+      });
+      
+      $scope.$on('session:tenant:changed', function () {
+        vm.loadHours();
+      });
+      
       $scope.$on('table:on:click:create', function() {
         $scope.selectedHour = new BusinessHour({
           tenantId: Session.tenant.tenantId,
           active: true
         });
       });
-
+      
       $scope.tableConfig = hoursTableConfig;
+      $scope.isHoursCustom = false;
       $scope.forms = {};
+      
       vm.loadTimezones();
       vm.loadHours();
     }
