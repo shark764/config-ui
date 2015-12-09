1cb8bc73a908e696363584f0850568de86b01626
Phil Hachey <phil.hachey@bluespurs.com>
2015-12-09 13:18:53 -0400

    TITAN2-4975 added more save and validation logic

--- src/app/components/configuration/hours/hoursController.js
+++ src/app/components/configuration/hours/hoursController.js
@@ -1,70 +1,100 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .controller('HoursController', ['$scope', '$filter', 'Session', 'BusinessHour', 'BusinessHourException', 'Timezone', 'hoursTableConfig', 'apiHostname',
-    function($scope, $filter, Session, BusinessHours, BusinessHourException, Timezone, hoursTableConfig) {
+  .controller('HoursController', ['$scope', '$filter', 'Session', 'BusinessHour', 'BusinessHourException', 'Timezone', 'hoursTableConfig', 'Alert',
+    function($scope, $filter, Session, BusinessHour, BusinessHourException, Timezone, hoursTableConfig, Alert) {
       var vm = this;
-      
+
       vm.loadTimezones = function() {
         $scope.timezones = Timezone.query();
       };
-      
+
       vm.loadHours = function() {
-        $scope.hours = BusinessHours.cachedQuery({
+        $scope.hours = BusinessHour.cachedQuery({
           tenantId: Session.tenant.tenantId
         });
+
+        return $scope.hours;
       };
-      
+
       $scope.submit = function(){
-        return $scope.selectedHours.save();
+        return $scope.selectedHour.save();
       };
-      
+
       $scope.hasHours = function() {
-        for(var index in $scope.selectedHours) {
-          return index.indexOf('StartTimeMinutes') > -1 ||
-            index.indexOf('EndTimeMinutes') > -1;
+        for(var index in $scope.selectedHour) {
+          if(index.indexOf('StartTimeMinutes') > -1 ||
+            index.indexOf('EndTimeMinutes') > -1) {
+            return true;
+          }
         }
       };
-      
+
       $scope.showCreateException = function(){
-        $scope.exceptionHours = {};
+        //will probably need an external library to do this properly
+        var newLocalDate = new Date();
+        var newUTCDate = new Date(Date.UTC(
+          newLocalDate.getFullYear(), newLocalDate.getMonth(), newLocalDate.getDate(), 0, 0, 0, 0));
+
+        $scope.exceptionHour = new BusinessHourException({
+          date: newUTCDate,
+          isAllDay: true
+        });
       };
-      
-      $scope.addException = function(){
-        $scope.selectedHours.exceptions.push($scope.exceptionHours);
-        $scope.exceptionHours = null;
+
+      $scope.cancelException = function() {
+        $scope.exceptionHour = null;
       };
-      
+
+      $scope.submitException = function(){
+        return $scope.exceptionHour.save({
+          tenantId: Session.tenant.tenantId,
+          businessHourId: $scope.selectedHour.id
+        }).then(function(exceptionHour) {
+          $scope.selectedHour.$exceptions.push(exceptionHour);
+          $scope.exceptionHour = null;
+          Alert.success($filter('translate')('hours.exception.create.success'));
+          return exceptionHour;
+        }, function(error) {
+          Alert.success($filter('translate')('hours.exception.create.failure'));
+          return error;
+        });
+      };
+
+      $scope.removeException = function(exception) {
+        return exception.$delete({
+          businessHourId: $scope.selectedHour.id
+        }).then(function() {
+          $scope.selectedHour.$exceptions.removeItem(exception);
+          Alert.success($filter('translate')('hours.exception.removed.success'));
+        }, function() {
+          Alert.error($filter('translate')('hours.exception.removed.failure'));
+        });
+      };
+
       $scope.generateHoursMessage = function(day) {
         return {
           day: $filter('translate')('hours.' + day)
         };
       };
-      
+
+      $scope.formatMinutes = function(minutes) {
+        var hours = Math.floor(minutes / 60);
+        hours = hours < 10 ? '0' + hours : hours;
+
+        var minutesRemainder = minutes - (hours * 60);
+        minutesRemainder = minutesRemainder < 10 ? '0' + minutesRemainder : minutesRemainder;
+
+        return hours + ':' + minutesRemainder;
+      };
+
       $scope.$on('table:on:click:create', function() {
-        $scope.selectedHours = new BusinessHours({
+        $scope.selectedHour = new BusinessHour({
           tenantId: Session.tenant.tenantId,
           active: true
         });
       });
-      
-      $scope.hours = {};
-      $scope.$watch('hours', function (newHours) {
-        if(!newHours) {
-          return;
-        }
-        
-        for(var index in newHours) {
-          var newDate = newHours[index];
-          
-          if(newDate) {
-            $scope.selectedHours[index] = (newDate.getHours() * 60) + newDate.getMinutes();
-          } else {
-            delete $scope.selectedHours[index];
-          }
-        }
-      }, true);
-      
+
       $scope.tableConfig = hoursTableConfig;
       $scope.forms = {};
       vm.loadTimezones();
