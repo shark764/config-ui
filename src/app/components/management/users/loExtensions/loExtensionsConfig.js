6c2cb04df8cde367831732320954722228460bfc
Greg Royan <greg.royan@bluespurs.com>
2015-12-10 09:20:46 -0400

    Removed  and replaced with .instant

--- src/app/components/management/users/loExtensions/loExtensionsConfig.js
+++ src/app/components/management/users/loExtensions/loExtensionsConfig.js
@@ -1,24 +1,24 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .service('loExtensionProviders', ['$filter', function ($filter) {
+  .service('loExtensionProviders', ['$translate', function ($translate) {
     return [{
       value: 'twilio',
-      display: $filter('translate')('twilio')
+      display: $translate.instant('twilio')
     }, {
       value: 'plivo',
-      display: $filter('translate')('plivo')
+      display: $translate.instant('plivo')
     }];
   }])
-  .service('loExtensionTypes', ['$filter', function ($filter) {
+  .service('loExtensionTypes', ['$translate', function ($translate) {
     return [{
       value: 'webrtc',
-      display: $filter('translate')('webrtc')
+      display: $translate.instant('webrtc')
     }, {
       value: 'pstn',
-      display: $filter('translate')('pstn')
+      display: $translate.instant('pstn')
     }, {
       value: 'sip',
-      display: $filter('translate')('sip')
+      display: $translate.instant('sip')
     }];
   }]);
