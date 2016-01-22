a915236bb5935dad4572c0c683ca0d68caf29b2d
samantha.routledge <samantha.routledge@bluespurs.com>
2016-01-22 11:33:31 -0400

    Coverage

--- src/app/components/flows/dispatchMappings/dispatchMappingsController.spec.js
+++ src/app/components/flows/dispatchMappings/dispatchMappingsController.spec.js
@@ -69,6 +69,51 @@ describe('DispatchMappingsController', function() {
     });
   });
 
+  describe('ON isTelInput', function() {
+    it('should return falsy if no dispatchMapping selected', function() {
+      $scope.selectedDispatchMapping = null;
+
+      var result = $scope.isTelInput();
+      expect(result).toBeFalsy();
+    });
+
+    it('should return truthy if the selected dispatchMapping has an interactionField of "customer"', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        interactionField: 'customer'
+      });
+
+      var result = $scope.isTelInput();
+      expect(result).toBeTruthy();
+    }));
+    
+    it('should return truthy if the selected dispatchMapping has an interactionField of "contact-point"', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        interactionField: 'contact-point'
+      });
+
+      var result = $scope.isTelInput();
+      expect(result).toBeTruthy();
+    }));
+    
+    it('should return falsy if the selected dispatchMapping has an interactionField of "source"', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        interactionField: 'source'
+      });
+
+      var result = $scope.isTelInput();
+      expect(result).toBeFalsy();
+    }));
+    
+    it('should return falsy if the selected dispatchMapping has an interactionField of "direction"', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        interactionField: 'direction'
+      });
+
+      var result = $scope.isTelInput();
+      expect(result).toBeFalsy();
+    }));
+  });
+
   describe('ON loadIntegrations', function() {
     it('should be defined on controller', function() {
       expect(controller.loadIntegrations);
@@ -88,5 +133,122 @@ describe('DispatchMappingsController', function() {
   it('should have a function to create a new dispatchMapping and set it as selected', function() {
     $scope.$broadcast(loEvents.tableControls.itemCreate);
     expect($scope.selectedDispatchMapping).toBeDefined();
+  });
+
+  describe('submit function', function() {
+    it('should be defined on scope', function() {
+      expect($scope.submit).toBeDefined();
+    });
+
+    it('should save the selected dispatch mapping if it is new', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        tenantId: 'tenant-id'
+      });
+
+      $httpBackend.expectPOST(apiHostname + '/v1/tenants/tenant-id/dispatch-mappings').respond(201);
+      $scope.submit();
+      $httpBackend.flush();
+    }));
+
+    it('should update the selected dispatch mapping if it already exists', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        tenantId: 'tenant-id',
+        id: '12345'
+      });
+
+      $httpBackend.expectPUT(apiHostname + '/v1/tenants/tenant-id/dispatch-mappings/12345').respond(200);
+      $scope.submit();
+      $httpBackend.flush();
+    }));
+  });
+  
+  describe('updateActive function', function() {
+    it('should save the dispatch mapping', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        tenantId: 'myTenant',
+        active: false,
+        id: '1234'
+      });
+      $scope.selectedDispatchMapping.$original = $scope.selectedDispatchMapping;
+
+      $httpBackend.expectPUT(apiHostname + '/v1/tenants/myTenant/dispatch-mappings/1234').respond(200);
+      $scope.updateActive();
+
+      $httpBackend.flush();
+    }));
+    
+    it('should toggle the active property to true when it is false', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        tenantId: 'myTenant',
+        active: false,
+        id: '1234'
+      });
+      $scope.selectedDispatchMapping.$original = $scope.selectedDispatchMapping;
+
+      $httpBackend.expectPUT(apiHostname + '/v1/tenants/myTenant/dispatch-mappings/1234', {
+        active: true
+      }).respond(200);
+      $scope.updateActive();
+
+      $httpBackend.flush();
+    }));
+    
+    it('should toggle the active property to false when it is true', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        tenantId: 'myTenant',
+        active: true,
+        id: '1234'
+      });
+      $scope.selectedDispatchMapping.$original = $scope.selectedDispatchMapping;
+
+      $httpBackend.expectPUT(apiHostname + '/v1/tenants/myTenant/dispatch-mappings/1234', {
+        active: false
+      }).respond(200);
+      $scope.updateActive();
+
+      $httpBackend.flush();
+    }));
+
+    it('should update only the active status', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        tenantId: 'myTenant',
+        active: false,
+        id: '1234',
+        anotherProperty: 'somevalue'
+      });
+
+      $scope.selectedDispatchMapping.$original = $scope.selectedDispatchMapping;
+      $httpBackend.expectPUT(apiHostname + '/v1/tenants/myTenant/dispatch-mappings/1234', {
+        active: true
+      }).respond(200);
+      $scope.updateActive();
+
+      $httpBackend.flush();
+    }));
+
+    it('should update the $original value on success', inject(function(DispatchMapping) {
+      $scope.selectedDispatchMapping = new DispatchMapping({
+        tenantId: 'myTenant',
+        active: false,
+        id: '1234'
+      });
+
+      $scope.selectedDispatchMapping.$original = angular.copy($scope.selectedDispatchMapping);
+      expect($scope.selectedDispatchMapping.$original.active).toBeFalsy();
+
+      $httpBackend.expectPUT(apiHostname + '/v1/tenants/myTenant/dispatch-mappings/1234').respond(200, {
+        result: {
+          tenantId: 'myTenant',
+          active: true,
+          id: '1234'
+        }
+      });
+      
+      $scope.updateActive();
+
+      $httpBackend.flush();
+      
+      expect($scope.selectedDispatchMapping.$original.active).toBeTruthy();
+    }));
   });
 });
