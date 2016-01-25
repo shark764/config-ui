f29b05ea3b58e07505c4c6bb7e7c06b5a10b6f10
samantha.routledge <samantha.routledge@bluespurs.com>
2016-01-25 16:58:32 -0400

    TITAN2-6698 Add ability to edit extensions

---
+++ src/app/components/management/users/loExtensions/loExtensionEditorDirective.spec.js
@@ -0,0 +1,140 @@
+'use strict';
+
+describe('loExtensionEditor directive', function(){
+  var isolateScope;
+  
+  beforeEach(module('liveopsConfigPanel'));
+  beforeEach(module('gulpAngular'));
+
+  beforeEach(inject(['$compile', '$rootScope', function($compile,$rootScope) {
+    var $scope = $rootScope.$new();
+    $scope.extension = {type: 'webrtc'};
+
+    var element = $compile('<ng-form><lo-extension-editor extension="extension"></lo-extension-editor></ng-form>')($scope);
+    $scope.$digest();
+    
+    isolateScope = element.find('lo-extension-editor').isolateScope();
+    isolateScope.form = {
+      type: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine')
+      },
+      provider: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine')
+      },
+      value: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine')
+      },
+      sipValue: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine')
+      },
+      telValue: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine')
+      },
+      extensiondescription: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine')
+      },
+      extensions: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine')
+      },
+      activeExtension: {
+        $setPristine: jasmine.createSpy('$setPristine'),
+        $setUntouched: jasmine.createSpy('$setUntouched'),
+        $setValidity: jasmine.createSpy('$setPristine'),
+        $setDirty: jasmine.createSpy('$setDirty')
+      },
+      loFormSubmitController: {
+        populateApiErrors: jasmine.createSpy('populateApiErrors')
+      }
+    };
+  }]));
+
+  describe('clearValues function', function() {
+    it('should reset the form state', function() {
+      isolateScope.phoneNumber = '12345678';
+      isolateScope.phoneExtension = '1234';
+      isolateScope.clearValues();
+
+      expect(isolateScope.phoneNumber).toBeNull();
+      expect(isolateScope.phoneExtension).toBeNull();
+      expect(isolateScope.sipExtension).toBeNull();
+
+      expect(isolateScope.form.type.$setPristine).toHaveBeenCalled();
+      expect(isolateScope.form.type.$setUntouched).toHaveBeenCalled();
+      expect(isolateScope.form.provider.$setPristine).toHaveBeenCalled();
+      expect(isolateScope.form.provider.$setUntouched).toHaveBeenCalled();
+      expect(isolateScope.form.sipValue.$setPristine).toHaveBeenCalled();
+      expect(isolateScope.form.sipValue.$setUntouched).toHaveBeenCalled();
+      expect(isolateScope.form.telValue.$setPristine).toHaveBeenCalled();
+      expect(isolateScope.form.telValue.$setUntouched).toHaveBeenCalled();
+      expect(isolateScope.form.extensiondescription.$setPristine).toHaveBeenCalled();
+      expect(isolateScope.form.extensiondescription.$setUntouched).toHaveBeenCalled();
+    });
+  });
+  
+  describe('updateExtension function', function() {
+    it('should add the pstn extension to the number, if given', function() {
+      isolateScope.phoneNumber = 'mynewnumber';
+      isolateScope.phoneExtension = '1234';
+      isolateScope.updateExtension();
+      expect(isolateScope.extension.value).toEqual('mynewnumberx1234');
+    });
+
+    it('should set the sip value, if given', function() {
+      isolateScope.sipExtension = 'sip:user@example.com';
+      isolateScope.updateExtension();
+      expect(isolateScope.extension.value).toEqual('sip:user@example.com');
+    });
+  });
+
+  describe('updateDisplay function', function() {
+    it('should set the $scope.phoneNumber value if the extension is a plain phone number', function() {
+      isolateScope.extension = {
+        value: '+1-506-555-5555',
+        type: 'pstn'
+      };
+
+      isolateScope.updateDisplay();
+      expect(isolateScope.phoneNumber).toEqual('+1-506-555-5555');
+      expect(isolateScope.sipExtension).toBeFalsy();
+      expect(isolateScope.phoneExtension).toBeFalsy();
+    });
+
+    it('should set the $scope.phoneNumber and $scope.phoneExtension values if given a phone number with extension', function() {
+      isolateScope.extension = {
+        value: '+1-506-555-5555x1234',
+        type: 'pstn'
+      };
+
+      isolateScope.updateDisplay();
+      expect(isolateScope.phoneNumber).toEqual('+1-506-555-5555');
+      expect(isolateScope.sipExtension).toBeFalsy();
+      expect(isolateScope.phoneExtension).toEqual('1234');
+    });
+
+    it('should set the $scope.sipExtension value if given a sip extension', function() {
+      isolateScope.extension = {
+        value: 'sip:test@example.com',
+        type: 'sip'
+      };
+
+      isolateScope.updateDisplay();
+      expect(isolateScope.phoneNumber).toBeFalsy();
+      expect(isolateScope.sipExtension).toEqual('sip:test@example.com');
+      expect(isolateScope.phoneExtension).toBeFalsy();
+    });
+  });
+});
--- src/app/components/management/users/loExtensions/loExtensionsController.spec.js
+++ src/app/components/management/users/loExtensions/loExtensionsController.spec.js
@@ -16,53 +16,6 @@ describe('loExtensions controller', function() {
     $scope.tenantUser = mockTenantUsers[0];
     $scope.tenantUser.extensions = [];
 
-    $scope.form = {
-      type: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine')
-      },
-      provider: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine')
-      },
-      value: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine')
-      },
-      sipValue: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine')
-      },
-      telValue: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine')
-      },
-      extensiondescription: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine')
-      },
-      extensions: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine')
-      },
-      activeExtension: {
-        $setPristine: jasmine.createSpy('$setPristine'),
-        $setUntouched: jasmine.createSpy('$setUntouched'),
-        $setValidity: jasmine.createSpy('$setPristine'),
-        $setDirty: jasmine.createSpy('$setDirty')
-      },
-      loFormSubmitController: {
-        populateApiErrors: jasmine.createSpy('populateApiErrors')
-      }
-    };
-
     controller = $controller('loExtensionsController', {
       '$scope': $scope
     });
@@ -77,21 +30,14 @@ describe('loExtensions controller', function() {
       $httpBackend.flush();
     }));
 
-    it('should reset the form on success', inject(function($httpBackend, apiHostname) {
+    it('should reset the form state on success', inject(function($httpBackend, apiHostname) {
       $httpBackend.expectPUT(apiHostname + '/v1/tenants/tenant-id/users/userId1').respond(200);
       controller.save();
       $httpBackend.flush();
 
-      expect($scope.form.type.$setPristine).toHaveBeenCalled();
-      expect($scope.form.type.$setUntouched).toHaveBeenCalled();
-      expect($scope.form.provider.$setPristine).toHaveBeenCalled();
-      expect($scope.form.provider.$setUntouched).toHaveBeenCalled();
-      expect($scope.form.sipValue.$setPristine).toHaveBeenCalled();
-      expect($scope.form.sipValue.$setUntouched).toHaveBeenCalled();
-      expect($scope.form.telValue.$setPristine).toHaveBeenCalled();
-      expect($scope.form.telValue.$setUntouched).toHaveBeenCalled();
-      expect($scope.form.extensiondescription.$setPristine).toHaveBeenCalled();
-      expect($scope.form.extensiondescription.$setUntouched).toHaveBeenCalled();
+      expect($scope.newExtension.type).toEqual('webrtc');
+      expect(controller.editingExtension).toBeNull();
+      expect($scope.creatingExtension).toBeFalsy();
     }));
 
     it('should reset the user on fail', inject(function($httpBackend, apiHostname) {
@@ -119,46 +65,48 @@ describe('loExtensions controller', function() {
     it('should add to the user\'s extensions', inject(function($q) {
       spyOn(controller, 'save').and.returnValue($q.when());
 
-      $scope.phoneNumber = 'mynewnumber';
+      $scope.newExtension = {
+        value: 'mynewnumber'
+      };
       $scope.add();
       expect($scope.tenantUser.extensions.length).toBe(1);
       expect($scope.tenantUser.extensions[0].value).toEqual('mynewnumber');
     }));
 
-    it('should add the pstn extension to the number, if given', inject(function($q) {
-      spyOn(controller, 'save').and.returnValue($q.when());
-
-      $scope.phoneNumber = 'mynewnumber';
-      $scope.phoneExtension = '1234';
-      $scope.add();
-      expect($scope.tenantUser.extensions.length).toBe(1);
-      expect($scope.tenantUser.extensions[0].value).toEqual('mynewnumberx1234');
-    }));
-
-    it('should set the sip value, if given', inject(function($q) {
-      spyOn(controller, 'save').and.returnValue($q.when());
-
-      $scope.sipExtension = 'sip:user@example.com';
-      $scope.add();
-      expect($scope.tenantUser.extensions.length).toBe(1);
-      expect($scope.tenantUser.extensions[0].value).toEqual('sip:user@example.com');
-    }));
-
-    it('should reset the phone number values on success', inject(function($q) {
-      spyOn(controller, 'save').and.returnValue($q.when());
-
-      $scope.phoneNumber = '12345';
-      $scope.phoneExtension = '123';
-      $scope.sipExtension = 'aaaa';
-      $scope.newExtension.provider = '4566';
-      $scope.add();
-      $scope.$digest();
-
-      expect($scope.phoneNumber).toBeFalsy();
-      expect($scope.phoneExtension).toBeFalsy();
-      expect($scope.sipExtension).toBeFalsy();
-      expect($scope.newExtension.provider).toBeFalsy();
-    }));
+//    it('should add the pstn extension to the number, if given', inject(function($q) {
+//      spyOn(controller, 'save').and.returnValue($q.when());
+//
+//      $scope.phoneNumber = 'mynewnumber';
+//      $scope.phoneExtension = '1234';
+//      $scope.add();
+//      expect($scope.tenantUser.extensions.length).toBe(1);
+//      expect($scope.tenantUser.extensions[0].value).toEqual('mynewnumberx1234');
+//    }));
+//
+//    it('should set the sip value, if given', inject(function($q) {
+//      spyOn(controller, 'save').and.returnValue($q.when());
+//
+//      $scope.sipExtension = 'sip:user@example.com';
+//      $scope.add();
+//      expect($scope.tenantUser.extensions.length).toBe(1);
+//      expect($scope.tenantUser.extensions[0].value).toEqual('sip:user@example.com');
+//    }));
+//
+//    it('should reset the phone number values on success', inject(function($q) {
+//      spyOn(controller, 'save').and.returnValue($q.when());
+//
+//      $scope.phoneNumber = '12345';
+//      $scope.phoneExtension = '123';
+//      $scope.sipExtension = 'aaaa';
+//      $scope.newExtension.provider = '4566';
+//      $scope.add();
+//      $scope.$digest();
+//
+//      expect($scope.phoneNumber).toBeFalsy();
+//      expect($scope.phoneExtension).toBeFalsy();
+//      expect($scope.sipExtension).toBeFalsy();
+//      expect($scope.newExtension.provider).toBeFalsy();
+//    }));
   });
 
   describe('remove function', function() {
