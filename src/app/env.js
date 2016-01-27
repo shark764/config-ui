3cd56404c3604dcc84bc80f428b1fab766667c7b
josh-stevens <jcstevens12@outlook.com>
2016-01-27 16:16:33 -0600

    Revert to original API in env.js

--- src/app/env.js
+++ src/app/env.js
@@ -2,8 +2,7 @@
 
 angular.module('liveopsConfigPanel.config')
   // .constant('apiHostname', 'http://localhost:9080')
-  // .constant('apiHostname', 'https://dev-api.liveopslabs.com')
-  .constant('apiHostname', 'https://qe-api.liveopslabs.com')
+  .constant('apiHostname', 'https://dev-api.liveopslabs.com')
   .constant('BIRST_URL', 'https://dev-birst.liveopslabs.com')
   .constant('SSO_PASSWORD', 'JO4IIiv0vuzyhoYoyWpbip0QquoCQyGh')
   .constant('SPACE_ID', '2846b565-23f8-4032-b563-21f8b7a01cc5')
