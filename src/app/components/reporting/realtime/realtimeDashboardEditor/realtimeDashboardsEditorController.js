86861ffe07d349a550a178f253379891581b0708
josh-stevens <jcstevens12@outlook.com>
2016-01-27 11:56:58 -0600

    Add selector to realtime dashboards

--- src/app/components/reporting/realtime/realtimeDashboardEditor/realtimeDashboardsEditorController.js
+++ src/app/components/reporting/realtime/realtimeDashboardEditor/realtimeDashboardsEditorController.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-.controller('realtimeDashboardsEditorController', ['$scope', 'dashboard', 'queues', 'users',
-  function($scope, dashboard, queues, users) {
+.controller('realtimeDashboardsEditorController', ['$scope', 'dashboard', 'queues', 'users', 'dashboards', '$state',
+  function($scope, dashboard, queues, users, dashboards, $state) {
     $scope.queues = queues;
     _.each(users, function (u) { u.name = u.firstName + ' ' + u.lastName; });
     $scope.users = users;
     $scope.dashboard = dashboard;
+    $scope.dashboards = dashboards;
   }
 ]);
