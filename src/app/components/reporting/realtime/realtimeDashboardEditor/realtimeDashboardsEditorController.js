ad5112659fef6894c05ab5b1ff8f2df46cce35ee
josh-stevens <jcstevens12@outlook.com>
2016-01-27 14:56:54 -0600

    TITAN2-7134 - Add dashboard selector to realtime dashboards page

--- src/app/components/reporting/realtime/realtimeDashboardEditor/realtimeDashboardsEditorController.js
+++ src/app/components/reporting/realtime/realtimeDashboardEditor/realtimeDashboardsEditorController.js
@@ -1,12 +1,18 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-.controller('realtimeDashboardsEditorController', ['$scope', 'dashboard', 'queues', 'users', 'dashboards', '$state',
-  function($scope, dashboard, queues, users, dashboards, $state) {
+.controller('realtimeDashboardsEditorController', ['$scope', 'dashboard', 'queues', 'users', 'dashboards', '$state', 'FullscreenService',
+  function($scope, dashboard, queues, users, dashboards, $state, FullscreenService) {
     $scope.queues = queues;
     _.each(users, function (u) { u.name = u.firstName + ' ' + u.lastName; });
     $scope.users = users;
     $scope.dashboard = dashboard;
     $scope.dashboards = dashboards;
+    $scope.toggleFullscreen = FullscreenService.toggleFullscreen;
+
+    $scope.$watch('selectedDashboard', function(newValue, oldValue) {
+      if (newValue === oldValue || oldValue === undefined) { return; }
+      $state.go('content.realtime-dashboards-management.editor', {id: newValue.id}, {reload:true})
+    });
   }
 ]);
