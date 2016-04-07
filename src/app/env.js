1def6f2ae92f91bebd0a9d7d3f597b95fed27dca
kirdua1 <kcraft@liveops.com>
2016-04-07 07:20:24 -0500

    squash commit

--- src/app/env.js
+++ src/app/env.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('liveopsConfigPanel.config')
-  // .constant('apiHostname', 'http://localhost:9080')
+  //.constant('apiHostname', 'http://localhost:9080')
   .constant('apiHostname', 'https://dev-api.liveopslabs.com')
   .constant('BIRST_URL', 'https://dev-birst.liveopslabs.com')
   .constant('SSO_PASSWORD', 'JO4IIiv0vuzyhoYoyWpbip0QquoCQyGh')
