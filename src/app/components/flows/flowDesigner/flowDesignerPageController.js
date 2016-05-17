4f552316c4c6fc8b40b74c55cd82301262c0eea2
Nick Fitzpatrick <nickfitz.582@gmail.com>
2016-05-17 16:48:44 -0300

    removing some changes for deployment to QE

--- src/app/components/flows/flowDesigner/flowDesignerPageController.js
+++ src/app/components/flows/flowDesigner/flowDesignerPageController.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('liveopsConfigPanel')
-  .controller('DesignerPageController', ['$scope', 'flow', 'notations', 'draft', 'platformFlow', 'FlowResource', 'FlowNotationService', 'FlowLibrary', 'lodash',
-    function($scope, flow, notations, draft, platformFlow, FlowResource, FlowNotationService, FlowLibrary, lodash) {
+  .controller('DesignerPageController', ['$scope', 'flow', 'notations', 'draft', 'FlowResource', 'FlowNotationService', 'FlowLibrary', 'lodash',
+    function($scope, flow, notations, draft, FlowResource, FlowNotationService, FlowLibrary, lodash) {
       $scope.flow = flow;
       $scope.draft = draft;
 
-      FlowResource.setPlatformFlow(platformFlow);
+      // FlowResource.setPlatformFlow(platformFlow);
 
       if(flow.type === 'customer' || flow.type === 'reusable'){
         FlowNotationService.setLastParticipant('titan/customer');
@@ -14,7 +14,8 @@ angular.module('liveopsConfigPanel')
 
       var parsedNotations = FlowNotationService.parseNotations(notations);
 
-      FlowLibrary.loadData(parsedNotations, platformFlow);
+      // FlowLibrary.loadData(parsedNotations, platformFlow);
+      FlowLibrary.loadData(parsedNotations);
 
       lodash.each(FlowResource.getFlows(), function(flow){
         var opts = _.findWhere(parsedNotations.flows, {flow: flow.id});
