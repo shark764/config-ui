d5c70ac04555ab77dc07e2c0e0dd6f1a9926fed7
Josh Clowater <jclowater@liveopscloud.com>
2016-12-08 15:07:34 -0400

    Hide API keys configuration menu item.

--- src/app/components/navbar/navbarController.js
+++ src/app/components/navbar/navbarController.js
@@ -231,14 +231,14 @@ angular.module('liveopsConfigPanel')
         }
         // ...END OUTBOUND FEATURE FLAG
 
-        if (UserPermissions.hasPermissionInList(PermissionGroups.viewAppCreds)) {
-          items.push({
-            label: $translate.instant('navbar.configuration.keys.title'),
-            stateLink: 'content.configuration.keys',
-            id: 'key-configuration-link',
-            order: 7
-          });
-        }
+        // if (UserPermissions.hasPermissionInList(PermissionGroups.viewAppCreds)) {
+        //   items.push({
+        //     label: $translate.instant('navbar.configuration.keys.title'),
+        //     stateLink: 'content.configuration.keys',
+        //     id: 'key-configuration-link',
+        //     order: 7
+        //   });
+        // }
 
         if (UserPermissions.hasPermission('VIEW_ALL_TRANSFER_LISTS')) {
           items.push({
