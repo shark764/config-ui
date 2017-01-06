f351a4920792117b1fb0eabc4eabbfb47990a27f
Doron Orenstein <jazziseverywhere@gmail.com>
2017-01-06 11:36:30 -0600

    CXV1-7123 - On Integrations page, added 3rd option for authentication method, 'no authentication'

--- src/app/components/configuration/integrations/bulkActions/integrationStatus/setIntegrationStatusBulkActionDirective.js
+++ src/app/components/configuration/integrations/bulkActions/integrationStatus/setIntegrationStatusBulkActionDirective.js
@@ -5,7 +5,10 @@ angular.module('liveopsConfigPanel')
     function(Integration, Session, BulkAction, statuses) {
       return {
         restrict: 'E',
-        scope: {},
+        scope: {
+          authType: '=',
+          authMethodCopy: '='
+        },
         require: '?^bulkActionExecutor',
         templateUrl: 'app/components/configuration/integrations/bulkActions/integrationStatus/setIntegrationStatusBulkAction.html',
         link: function($scope, elem, attr, bulkActionExecutor) {
@@ -29,8 +32,9 @@ angular.module('liveopsConfigPanel')
             return integrationCopy.save().then(function(integrationCopy) {
               angular.copy(integrationCopy, integration);
               integration.checked = true;
+
               // we'll be using this to reset the auth method select after saving
-              integrationCopy.handleAuthMethodSelect(tempSelectedIntegration, 'true');
+              integrationCopy.handleAuthMethodSelect(tempSelectedIntegration, $scope.authType, $scope.authMethodCopy, 'true');
               return integration;
             });
           };
