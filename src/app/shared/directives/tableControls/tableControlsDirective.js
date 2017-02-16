ca9682a71eb0b38e219bbd035b4e071ed2347548
Doron Orenstein <jazziseverywhere@gmail.com>
2017-02-16 07:16:00 -0600

    stopped working, changed  to , but unit tests are failing

--- src/app/shared/directives/tableControls/tableControlsDirective.js
+++ src/app/shared/directives/tableControls/tableControlsDirective.js
@@ -17,8 +17,8 @@ angular.module('liveopsConfigPanel')
    *  * title (string): The title of the page. Also used as the key for storing the user's column preferences. Defaults to undefined
    */
 
-  .directive('tableControls', ['$rootScope', 'loEvents', 'DirtyForms', 'Session', '$timeout',
-    function($rootScope, loEvents, DirtyForms, Session, $timeout) {
+  .directive('tableControls', ['$rootScope', 'loEvents', 'DirtyForms', 'Session', '$q',
+    function($rootScope, loEvents, DirtyForms, Session, $q) {
       return {
         restrict: 'E',
         scope: {
@@ -63,8 +63,8 @@ angular.module('liveopsConfigPanel')
             setColumnPreferences();
           });
 
-          $scope.$on('dropdown:item:checkedUncheckedAll', function () {
-            $timeout(function () {
+          $scope.$on('dropdown:item:checkedUncheckedAll', function (event, emmittedData) {
+            $q.when(emmittedData).then(function () {
               setColumnPreferences();
             });
           });
