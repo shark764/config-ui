caf2797ce09994335199491a9865acfa87eec8e6
Wooseok Paul Lee <111leews@gmail.com>
2019-06-10 13:25:10 -0300

    CXV1-18160 Create new Config UI navigation and pages for Platform Support Tools Added constant on env.js to prevent from having error

--- src/app/components/supportTool/flowDebugger/flowDebuggerPageController.js
+++ src/app/components/supportTool/flowDebugger/flowDebuggerPageController.js
@@ -5,33 +5,28 @@ angular.module('liveopsConfigPanel')
     function($scope, $window, $state, $sce, Session, UserPermissions, PermissionGroups, apiHostname, debuggerHostname) {
       /* globals window */
 
-      if (UserPermissions.hasPermissionInList(PermissionGroups.viewDebugTool)) {
-        $scope.debuggerHostname = $sce.trustAsResourceUrl(debuggerHostname);
+      $scope.debuggerHostname = $sce.trustAsResourceUrl(debuggerHostname);
 
-        function ProcessMessage(event) {
-          switch(event.data.message) {
-            case 'FlowDebugger.ready':
-            event.source.focus();
-            event.source.postMessage({
-              message: 'FlowDebugger.start',
-              data: {
-                apiToken: Session.token,
-                apiHostname: apiHostname
-              }
-            }, '*');
-            break;
-          }
+      function ProcessMessage(event) {
+        switch(event.data.message) {
+          case 'FlowDebugger.ready':
+          event.source.focus();
+          event.source.postMessage({
+            message: 'FlowDebugger.start',
+            data: {
+              apiToken: Session.token,
+              apiHostname: apiHostname
+            }
+          }, '*');
+          break;
         }
-        
-        window.addEventListener('message', ProcessMessage);
-
-        $scope.$on('$destroy', function() {
-          window.removeEventListener('message', ProcessMessage);
-        })
-      } else {
-        alert('You do not have permission to use Flow Debugger.');
-        location.reload();
       }
-    }
-    
+      
+      window.addEventListener('message', ProcessMessage);
+
+      $scope.$on('$destroy', function() {
+        window.removeEventListener('message', ProcessMessage);
+      })
+      
+    } 
 ]);
